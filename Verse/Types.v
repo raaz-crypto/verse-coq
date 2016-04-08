7c502656569741e90af70961fcfec3336084be3b
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-04-08 19:23:39 +0530

    changes to the tactics of types.

--- Verse/Types.v
+++ Verse/Types.v
@@ -5,36 +5,30 @@
 Most cryptographic primitives use datatypes that are relatively
 simple.  In verse, we have
 
-1. The word types. Word types are bounded in size in that the size
-   required to store them is know.
+1. Word types.
 
-2. Arrays of word types: Arrays are also bounded as their length
-   necessarily have to be a constant. Arrays are the only means for
-   accessing the main memory in verse and should be of a fixed
-   dimension. They are contiguous locations of basic word types. To
-   avoid endian confusion while loading and storing, they also need to
-   specify the endianness.
+2. Array types
 
-
-3. A sequence of bound types. Often we need to process a stream of
-   inputs each of which are bounded. A sequence type is for this
-   purpose. Verse forbids nested sequences as sequences are unbounded.
+3. Sequence types.
 
 *)
 
 
+Require Import Verse.Tactics.
+
 Inductive endian : Type := bigE | littleE | hostE.
 
+
 (** The abstract syntax of verse types *)
+
 Inductive type : Type :=
   | word       : nat  -> type
   | array      : nat  -> endian -> type -> type
   | sequence   : type -> type.
 
-
 (**
 
-Not all types are valid. The next
+Some common types are defined below.
 
  *)
 
@@ -43,11 +37,20 @@ Definition Byte   : type := Word8.
 Definition Word16 : type := word 16.
 Definition Word32 : type := word 32.
 
+(** ** Properties of types.
+
+This module contains various properties of types.
+
+
+*)
+
 Module Properties.
 
+(** Propertie asserting that a given type is a scalar *)
 Inductive Scalar : type -> Prop :=
   | ScalarWord {n : nat} : Scalar (word n).
 
+(** Property asserting that a given type is a vector *)
 Inductive Bounded          : type -> Prop :=
   | BoundedScalar {t : type}: Scalar t -> Bounded t
   | BoundedArray  {n : nat }
@@ -55,6 +58,7 @@ Inductive Bounded          : type -> Prop :=
                   {e : endian}
     : Scalar t -> Bounded (array n e t).
 
+(** Property asserting that a given type is well formed *)
 Inductive WellFormed      : type -> Prop :=
   | WFBound    {t : type} : Bounded t -> WellFormed t
   | WFSeq      {t : type} : Bounded t -> WellFormed (sequence t).
@@ -63,55 +67,78 @@ End Properties.
 
 (**
 
-Some common types.
+** Type tactics.
 
- *)
+Often during proof developement, we need to prove various properties
+about types. The tactics [scalar], [bounded] and [wellformed] are
+precisely for this. The tactics [types] is a generic tactic that
+proves all the above properties.
 
-(**
 
-Proving well formedness of types can be bothersome. This module
-exposes some tactics to automatically dispose of some of these
-obligations.
+Often we only want to ensure that a type has a given property; the
+[ensure_]family of tactics are for this purpose. Like the tactic
+[ensure], these tactic do not leave the associated assertions in the
+hypothesis. They are mostly for asserting property and moving on.
+
 
 *)
 
 Module Tactics.
+  Import Properties.
+
+  (** Proves that a type is a scalar *)
+
+  Ltac scalar :=
+    idtac;(* For some mysterious reason idtac is required *)
+    match goal with
+      | [ |- context[Scalar (word _)] ] => exact ScalarWord
+      | [ |- context[Scalar _] ]        => compute; scalar
+      | _                               => fail 0 "scalar type"
+    end.
+
+  (** Proves that a type is bounded *)
+
+  Ltac bounded :=
+    idtac; (* For some mysterious reason idtac is required *)
+    match goal with
+      | [ |- Bounded (array _ _ _) ]
+        => apply BoundedArray; scalar
+      | [ |- Bounded (word _)      ]
+        => apply BoundedScalar; scalar
+      | [ |- Bounded _ ] => compute ; bounded
+      | _ => fail 1 "bounded type"
+    end.
+
+  (** Proves that a type is well formed *)
+  Ltac wellformed :=
+    idtac;(* For some mysterious reason idtac is required *)
+    match goal with
+      | [ |- WellFormed (sequence _) ] => apply WFSeq; bounded
+      | [ |- WellFormed (array _ _ _)] => apply WFBound; bounded
+      | [ |- WellFormed (word  _)    ] => apply WFBound; bounded
+      | [ |- WellFormed _           ] => compute ; wellformed
+      | _                              => fail 1 "wellformed type"
+    end.
+
+  (** Proves all properties about goals *)
+  Ltac types :=
+    idtac;(* For some mysterious reason idtac is required *)
+    match goal with
+      (*| [ ?S : type, ?T : type |- context[ S = T] ] => idtac*)
+      | [ |- context[ Scalar  _] ]
+        => scalar
+      | [ |- context[ Bounded _] ]
+        => bounded
+      | [ |- context[ WellFormed _]]
+        => wellformed
+    end.
 
   (**
 
- This tactic simplifies all type obligations.
-
    *)
 
-  Import Properties.
-
-  Ltac discharge_scalar_obligations := exact ScalarWord.
-
-  Ltac discharge_bounded_obligations :=
-    match goal with
-      | [ |- Bounded (array _ _ _) ] =>
-        apply BoundedArray; discharge_scalar_obligations
-      | _
-        => apply BoundedScalar; discharge_scalar_obligations
-    end.
-
-
-  Ltac discharge_wf_obligations :=
-    match goal with
-      | [ |- WellFormed (sequence _) ] =>
-        apply WFSeq; discharge_bounded_obligations
-      | _
-        => apply WFBound; discharge_bounded_obligations
-    end.
-
-  Ltac types :=
-    match goal with
-      | [ |- context[ Scalar  _] ]
-        => compute;  discharge_scalar_obligations
-      | [ |- context[ Bounded _] ]
-        => compute;  discharge_bounded_obligations
-      | [ |- context[ WellFormed _]]
-        => compute; discharge_wf_obligations
-    end.
+  Ltac ensure_scalar  t    := ensure (Scalar     t) scalar.
+  Ltac ensure_bounded t    := ensure (Bounded    t) bounded.
+  Ltac ensure_wellformed t := ensure (WellFormed t) wellformed.
 
 End Tactics.
