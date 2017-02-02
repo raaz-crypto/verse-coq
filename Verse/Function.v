808afa61e98f622ee52e5c63c2fbbb385e972950
Abhishek Dang <ahdang@iitk.ac.in>
2017-02-02 14:39:09 +0530

    function param and local cleanup attempt

--- Verse/Function.v
+++ Verse/Function.v
@@ -1,13 +1,30 @@
+Require Import Types.Internal.
+Require Import Language.
+Require Import String.
+Require Import Ascii.
+Require Import Fin.
 
 
-Definition statements := list statement.
+Module Type Function.
+  
+  Parameter name    : string.
 
-Definition varSpec    := sigT var.
-Definition context    := list varSpec.
+  Parameter np   :  nat.
+  Parameter nl  : nat.
+  Parameter param  : type -> Type.
+  Parameter local  : type -> Type.
 
-Record function : Type
-  := makeFunction { name    : string;
-                    params  : context;
-                    locals  : context; 
-                    body    : statements;
-                  }.
+  Check sigT.
+  
+  Parameter porder : {o : Fin.t np -> {ty : type & (param ty)} & (forall p : { ty : type & (param ty) }, exists i : Fin.t np, o i = p)}.
+
+  Parameter lorder : {o : Fin.t nl -> {ty : type & (local ty)} & (forall l : { ty : type & (local ty) }, exists i : Fin.t nl, o i = l)}.
+
+  Inductive fvar : type -> Type :=
+  | p : forall ty : type, param ty -> fvar ty
+  | l : forall ty : type, local ty -> fvar ty
+  .
+
+  Parameter setup   : block fvar.
+
+End Function.
