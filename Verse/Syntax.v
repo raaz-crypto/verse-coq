31355c9fae205dae0d0fa0f20fed53fc494bf2b2
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-02-09 15:40:57 +0530

    AST module instead of class.

--- Verse/Syntax.v
+++ Verse/Syntax.v
@@ -83,14 +83,102 @@ are types that allow mapping over its variables using a substitution.
 
 *)
 
-Class AST (syn : synT) :=
-  {
-    (** Change variables according to substitution **)
-    map         {u v}                               : subT u v -> syn u -> syn v;
+Module Type AST.
 
-    (** The identity law of map *)
-    identity    {u}                                 : map (idSubst u) = id;
+  (* The type of the underlying syntax tree *)
+  Parameter syn : synT.
 
-    (** The law of composition *)
-    composition {u v w}{g : subT v w}{f : subT u v} : compose (map g) (map f) = map (g << f)
-  }.
+  (* The function that maps a subsitution over the syntax tree *)
+  Parameter map : forall {u v : varT}, subT u v -> syn u -> syn v.
+
+
+  Axiom identity : forall {u}, map (idSubst u) = id.
+
+  Axiom composition : forall {u v w}{g : subT v w}{f : subT u v}, compose (map g) (map f) = map (g << f).
+
+End AST.
+
+(** * Tactic to crush AST proof obligations
+
+When defining the instance for AST, the identity and composition laws
+are often straight forward. This tactic crushes them.
+
+ *)
+
+Ltac crush_ast_obligations
+  := apply functional_extensionality;
+     let x := fresh "X" in intro x; destruct x; trivial.
+
+
+(** * Some examples. *)
+
+Inductive opt (v : varT) :=
+| defined (t : type) : v t -> opt v
+| undefined          : opt v.
+
+Arguments defined [v t].
+Arguments undefined [v].
+Notation "[ X ]" := (defined X).
+Notation "_|_"   := undefined.
+
+
+Module OptAST <: AST.
+
+  Definition syn := opt.
+
+  Definition map {u v}(f : subT u v) (ou : opt u) : opt v :=
+    match ou with
+    | [ xU ] => [ f _ xU ]
+    | _|_    => _|_
+    end.
+
+  Lemma identity {u}: map (idSubst u) = id.
+  Proof.
+    crush_ast_obligations.
+  Qed.
+
+  Lemma composition {u v w}{g : subT v w}{f : subT u v}: compose (map g) (map f) = map (g << f).
+  Proof.
+    crush_ast_obligations.
+  Qed.
+
+End OptAST.
+
+
+
+(** A small example of maping over Opt.
+
+
+<<
+
+Inductive U : varT :=
+| xU : U Word32
+| yU : U Word64
+.
+
+Inductive V : varT :=
+| xV : V Word32
+| yV : V Word64
+.
+
+Print xU.
+
+Print U.
+Print V.
+
+Definition fUV : subT U V :=
+  fun _ x => match x with
+             | xU => xV
+             | yU => yV
+             end.
+
+Import OptAST.
+
+
+
+Print OptAST.map.
+
+Compute (map fUV [xU]).
+
+>>
+*)
