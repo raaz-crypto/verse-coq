288e304a780ab06d8a35233c8526aa7684e2bc10
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-06-29 19:33:57 +0530

    cleanup the semantics of type module.

--- Verse/Types/Semantics.v
+++ Verse/Types/Semantics.v
@@ -1,4 +1,6 @@
 Require Import Verse.Types.Internal.
+
+(* begin hide *)
 Require Vector.
 Require Streams.
 Require Import Bvector.
@@ -6,29 +8,61 @@ Require Import Coq.Numbers.BinNums.
 Require Import Coq.ZArith.Zdigits.
 Require Import BinInt.
 Require Import Nat.
-(**
-
-We now give a semantics of type. The function typeDenote gives a definition from
-
- *)
-
 Open Scope nat_scope.
 
-(* n-bit word *)
+(* end hide *)
+
+(** * Sematics of types.
+
+Firstly, we define the type WORD to capture bit vectors.
+
+*)
+
+
+
 Inductive WORD (n : nat) : Type :=
 | WORD_BITS : Bvector n -> WORD n.
 
 Arguments WORD_BITS [n] _.
 
+
+(**
+
+The semantics of verse types is given by mapping it into types in
+coq. Notice that [word n] denotes words that are [2^n] bytes long.
+and hence [typeDenote (word n) is [WORD(2^(n) * 2 ^ 3)].
+
+*)
+Fixpoint typeDenote (t : type) : Type :=
+  match t with
+    | word   n      => WORD (2^(n+3))
+    | vector n tw   => Vector.t (typeDenote tw) (2^n)
+    | array  n _ tw => Vector.t (typeDenote tw) n
+    | sequence tw   => Streams.Stream (typeDenote tw)
+  end.
+
+(** ** Truncated numeric functions on WORD
+
+The [WORD n] type can be seen as integers modulo 2^n. Numerical
+functions can then be defined on the word type applying the function
+and truncating module [2^n]. The combinators [numBinOp] and
+[numUnaryOp] are helper functions to define such variants.
+
+*)
+
+(** This function lifts a numeric binary function  to WORDS *)
 Definition numBinOp {n} f  (x y : WORD n) : WORD n :=
   match x, y with
   | WORD_BITS xv, WORD_BITS yv => WORD_BITS (Z_to_binary n (f (binary_value n xv)(binary_value n yv)))
   end.
 
+(** This function lifts a numeric unary function to WORDS *)
 Definition numUnaryOp {n : nat} f (x : WORD n) : WORD n :=
   match x with
   | WORD_BITS xv => WORD_BITS (Z_to_binary n (f (binary_value n xv)))
   end.
+
+(** We also give a way to define bitwise operations on the WORD type *)
 
 Definition bitwiseBinOp {n : nat} f (x y : WORD n) : WORD n :=
   match x,y with
@@ -41,17 +75,7 @@ Definition bitwiseUnaryOp {n : nat} f (x : WORD n) : WORD n :=
   end.
 
 
-Fixpoint typeDenote (t : type) : Type :=
-  match t with
-    | word   n      => WORD (2^(n+3))                   (** 2^n bytes = 2^n * 2^3 bits *)
-    | vector n tw   => Vector.t (typeDenote tw) (2^n)
-    | array  n _ tw => Vector.t (typeDenote tw) n
-    | sequence tw   => Streams.Stream (typeDenote tw)
-  end.
-
-
-(** Meaning of the binary operator at at the given type_ *)
-
+(** We now give the meanings of numeric and bitwise functions when the act on the types in Verse *)
 
 Fixpoint numBinaryDenote (f : Z -> Z -> Z) t : typeDenote t -> typeDenote t -> typeDenote t :=
   match t as t0 return typeDenote t0 -> typeDenote t0 -> typeDenote t0 with
@@ -68,6 +92,23 @@ Fixpoint numUnaryDenote (f : Z -> Z)(t : type) : typeDenote t -> typeDenote t :=
   | array  n _ tw => Vector.map (numUnaryDenote f tw)
   | sequence tw   => Streams.map (numUnaryDenote f tw)
   end.
+
+Fixpoint bitwiseBinaryDenote (f : bool -> bool -> bool) t : typeDenote t -> typeDenote t -> typeDenote t :=
+  match t as t0 return typeDenote t0 -> typeDenote t0 -> typeDenote t0 with
+  | word n        => bitwiseBinOp f
+  | vector n tw   => Vector.map2 (bitwiseBinaryDenote f tw)
+  | array  n _ tw => Vector.map2 (bitwiseBinaryDenote f tw)
+  | sequence tw   => Streams.zipWith (bitwiseBinaryDenote f tw)
+  end.
+
+Fixpoint bitwiseUnaryDenote (f : bool -> bool)(t : type) : typeDenote t -> typeDenote t :=
+  match t as t0 return typeDenote t0 -> typeDenote t0 with
+  | word n        => bitwiseUnaryOp f
+  | vector n tw   => Vector.map (bitwiseUnaryDenote f tw)
+  | array  n _ tw => Vector.map (bitwiseUnaryDenote f tw)
+  | sequence tw   => Streams.map (bitwiseUnaryDenote f tw)
+  end.
+
 
 Definition plus  := numBinaryDenote Z.add.
 Definition minus := numBinaryDenote Z.sub.
