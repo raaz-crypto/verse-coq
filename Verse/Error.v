4b8b0bfa41bdb42ad844c96d9f4be610fda86fba
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-07-02 19:31:13 +0530

    introduced Haskell like applicative notation for error.

--- Verse/Error.v
+++ Verse/Error.v
@@ -1,14 +1,37 @@
-
-Definition ap {A B : Type}{Err : Prop}(f : A -> B) (y : A + {Err}) :=
-  match y with
-  | inleft  a    => inleft (f a)
-  | inright err  => inright err
-  end.
+Section Error.
+  Variable A   : Type.
+  Variable Err : Prop.
 
 
+  Section Apply.
+    Variable B   : Type.
+    Definition ap (f : A -> B) (y : A + {Err}) :=
+      match y with
+      | inleft  a    => inleft (f a)
+      | inright err  => inright err
+      end.
 
-Definition recover {A : Type}{Err : Prop}(x : A + {Err}) : if x then A else Err
-  := match x with
-     | inleft a => a
-     | inright b => b
-     end.
+    Definition apA (f : (A -> B) + {Err})(x : A + {Err}) :  B + {Err} :=
+      match f, x with
+      | inleft f , inleft x  => inleft (f x)
+      | inright e, _         => inright e
+      | _        , inright e => inright e
+      end.
+  End Apply.
+
+  Definition recover (x : A + {Err}) : if x then A else Err
+    := match x with
+       | inleft a => a
+       | inright b => b
+       end.
+
+End Error.
+
+
+Arguments ap [A Err B] _ _.
+Arguments apA [A Err B] _ _.
+Arguments recover [A Err] _.
+
+(* Haskell like applicative notation for errors *)
+Global Notation "F <$> A" := (ap  F A) (at level 40, left associativity).
+Global Notation "F <*> A" := (apA F A) (at level 40, left associativity).
