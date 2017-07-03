f53b471c0ed18ed6599ee23af3a9610544d6b3cb
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-07-03 19:42:36 +0530

    cleaned up the build script

--- scripts/opam-build.sh
+++ scripts/opam-build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+COQ_BUILD_DIR="coq-$COQ_VER"
+
 function init()
 {
     mkdir -p "coq-$COQ_VER"
-    opam init --root="coq-$COQ_VER" --no-setup --yes
-    eval `opam config env --root="coq-$COQ_VER"`
+    opam init --root="$COQ_BUILD_DIR" --no-setup --yes
+    eval `opam config env --root="$COQ_BUILD_DIR"`
     opam install "coq.$COQ_VER" --yes
     coq_makefile -f _CoqProject -o Makefile
 
@@ -12,16 +14,16 @@ function init()
 
 function build()
 {
-    eval `opam config env --root=coq-"$COQ_VER"`
+    eval `opam config env --root="$COQ_BUILD_DIR"`
+    coqtop -v
     make
     make html
 
 }
 
-export COQ_VER=$2
 
 case $1 in
     init) init; ;;
     build) build; ;;
-    *) echo unkonwn command 
+    *) echo "$0:error:" unknown command "$1" 
 esac
