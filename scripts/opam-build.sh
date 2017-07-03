1a193dd1ad6bb634b8785bb13984031d5fce1872
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-07-03 19:54:27 +0530

    build script prints out coq's version.

--- scripts/opam-build.sh
+++ scripts/opam-build.sh
@@ -7,6 +7,7 @@ function init()
     mkdir -p "coq-$COQ_VER"
     opam init --root="$COQ_BUILD_DIR" --no-setup --yes
     eval `opam config env --root="$COQ_BUILD_DIR"`
+    coqtop -v
     opam install "coq.$COQ_VER" --yes
     coq_makefile -f _CoqProject -o Makefile
 
