4ec08f1960cdedf35581065c0e8d04fd52098ed2
Abhishek Dang <ahdang@iitk.ac.in>
2017-08-09 17:22:58 +0530

    TestCode added.

---
+++ Verse/TestCode.v
@@ -0,0 +1,58 @@
+Require Import Verse.CArch.
+Require Import Verse.Function.
+Require Import Verse.Types.
+Require Import Verse.Types.Internal.
+Require Import Verse.Language.
+Require Import Verse.Syntax.
+Import Verse.CArch.CArchAux.
+
+Require Import String.
+Require Import List.
+Import ListNotations.
+
+Definition loopvar := Byte.
+Definition pl      := [Word16].
+Definition lv      := [ArrayT 3 hostE Word16].
+Definition rv      := [Word16; Word32].
+
+Ltac crush_typred ty := refine (exist _ ty _); unfold Ensembles.In; repeat constructor.
+
+Definition localReg : list {ty : type | Ensembles.In _ CArch.supportedTy ty}.
+  refine [_;_].
+  crush_typred Word16.
+  crush_typred Word32.
+Defined.
+
+Definition paramTypes : list {ty : type | Ensembles.In _ CArch.supportedTy ty}.
+  refine [_].
+  crush_typred Word16.
+Defined.
+
+Definition localVars : list {ty : type | Ensembles.In _ CArch.supportedTy ty}.
+  refine ([_]).
+  crush_typred (ArrayT 3 hostE Word16).
+Defined.
+
+Definition p : Ensembles.In _ CArch.supportedTy loopvar.
+  unfold Ensembles.In. repeat constructor.
+Defined.
+
+Definition test : func loopvar pl lv rv :=
+  fun v : varT =>
+    fun num : v Word16 =>
+      fun arr : v (ArrayT 3 hostE Word16) =>
+        fun (tmp : v Word16) (double : v Word32) =>
+          {|
+            name    := "test";
+            setup   := [var num <= var tmp <*> Language.index arr 0];
+            loop    := fun lv : v loopvar => [var num <=  var tmp <+> Language.index arr 1];
+            cleanup := []
+          |}.
+
+Definition lalloc : allocation CArch.var rv := (inRegister (cr Word16 "tmp"), (inRegister (cr Word32 "double"), tt)).
+
+Definition code : string :=
+  let '(f, fa) := allocate' Byte p paramTypes localVars localReg test lalloc in
+  CArch.generate f fa.
+
+Compute code.
