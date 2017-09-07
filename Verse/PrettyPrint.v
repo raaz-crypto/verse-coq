a3fc1351b047ec8d676bf11e01cf42e4cedaa097
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-09-07 18:53:46 +0530

    rework the pretty printer.

--- Verse/PrettyPrint.v
+++ Verse/PrettyPrint.v
@@ -2,17 +2,89 @@ Require Import String.
 Require Import Strings.Ascii.
 Require Import List.
 Require Import NPeano.
+Require Verse.Word.
 Import ListNotations.
+
+(* Wadler Pretty printer *)
+
+Inductive Doc :=
+| NilDoc : Doc
+| Text   : string -> Doc -> Doc
+| Line   : nat    -> Doc -> Doc
+.
+
+Definition empty : Doc := NilDoc.
+
+Definition text (s : string) : Doc := Text s empty.
+Definition line : Doc              := Line 0 empty.
+
+Fixpoint nest n (d : Doc) : Doc :=
+  match d with
+  | Text s dp => Text s (nest n dp)
+  | Line i dp => Line (n + i) (nest n dp)
+  | NilDoc => NilDoc
+  end.
+
+
+Fixpoint spaces (n : nat) : string :=
+  match n with
+  | 0    => ""
+  | S m  => " " ++ spaces m
+  end.
+
+Fixpoint layout (d : Doc) : string :=
+  let newline := String (ascii_of_nat 10) EmptyString in
+  match d with
+  | Text s dp => s ++ layout dp
+  | Line i dp => newline ++ spaces i ++ layout dp
+  | NilDoc    => ""
+  end.
+
+Fixpoint append (a b : Doc) : Doc :=
+  match a with
+  | Text s dp => Text s (append dp b)
+  | Line i dp => Line i (append dp b)
+  | _         => b
+  end.
+
+Fixpoint appendSpace (a b : Doc) : Doc :=
+  match b with
+  | NilDoc => a
+  | _ => match a with
+         | Text s NilDoc  => append (text (s ++ " ")) b
+         | Text s dp      => Text s (appendSpace dp b)
+         | Line i dp => Line i (appendSpace dp b)
+         | _         => b
+         end
+  end.
+
+Notation  "A <>  B" := (append A B).
+Notation  "A <+> B" :=  (appendSpace A B) (at level 70).
+
+Definition combine (docs : list Doc) : Doc := List.fold_left append docs empty.
+Definition between (b e : string) (doc : Doc) := text b <> doc <> text e.
+
+Fixpoint sepBy (s : string) (docs : list Doc) : Doc :=
+  match docs with
+  | []        => empty
+  | [x]       => x
+  | (x :: xs) => (x <> text s) <+> sepBy s xs
+  end.
+
+Fixpoint sepEndBy (s : string) (docs : list Doc) : Doc :=
+  List.fold_left appendSpace (List.map (fun x => x <> text s) docs) empty.
+
+Definition commaSep   := sepBy ",".
+Definition semiSep    := sepBy ";".
+Definition semiEndSep := sepEndBy ";".
+
+
+Definition paren   := between "(" ")".
+Definition bracket := between "[" "]".
+Definition brace   := between "{" "}".
 
 
 Module Internal.
-
-  Fixpoint intercalate (punct : string) (ws : list string) : string :=
-  match ws with
-  | []        => ""
-  | [x]       => x
-  | (x :: xs) => (x ++ punct) ++ intercalate punct xs
-  end.
 
   Definition digit (n : nat) : ascii :=
     match n with
@@ -45,150 +117,5 @@ Module Internal.
 
 End Internal.
 
-Definition Words       := list string.
-Definition Line : Set  := nat * Words.
-Definition Doc         := list Line.
-
-Definition line (ws : Words) : Line := (0,ws).
-Definition lines := List.map line.
-
-Definition indent n (l : Line)   : Line :=
-  match l with
-  | (m, ws) => (n + m, ws)
-  end.
-
-Definition nest  (n : nat): Doc -> Doc := List.map (indent n).
-
-Definition decimal (n : nat) : Words :=  [Internal.nat_to_str n].
-
-
-
-
-
-
-
-Class DOC (doc : Set) := {
-                  between  : string -> string -> doc -> doc;
-                  sepBy    : string -> doc -> doc;
-                  sepEndBy : string -> doc -> doc;
-                  toString : doc -> string
-                }.
-
-
-Section Utils.
-  Variable doc : Set.
-  Variable cls : DOC doc.
-  Definition comaSep      := sepBy ",".
-  Definition semiSep      := sepBy ";".
-  Definition semiSepEnd   := sepEndBy ";".
-  Definition paren  := between "(" ")".
-  Definition bracket := between "[" "]".
-  Definition braces  := between "{" "}".
-End Utils.
-
-Arguments comaSep    [doc cls] _.
-Arguments semiSep    [doc cls] _.
-Arguments semiSepEnd [doc cls] _.
-Arguments paren      [doc cls] _.
-Arguments bracket    [doc cls] _.
-Arguments braces     [doc cls] _.
-
-
-(* Pretty print operations on lines. *)
-
-Module PrettyWords.
-
-   Fixpoint endBy (e : string) (ws : list string) : list string :=
-    match ws with
-    | []  => [e]
-    | [x] => [(x ++ e)%string ]
-    | (x::xs) => x :: endBy e xs
-    end.
-
-  Definition bet (b e : string) (ws : list string) : list string :=
-    match ws with
-    | []        => [(b ++ e)%string]
-    | [x]       => [(b ++ x ++ e)%string]
-    | (x :: xs) => (b ++ x)%string :: endBy e xs
-    end.
-
-  Fixpoint sep (s : string) (ws : list string) : list string :=
-    match ws with
-    | w1 :: wsp =>
-      match wsp with
-      | [] => ws
-      | _  => (w1 ++ s)%string :: sep s wsp
-      end
-    | _               => ws
-    end.
-
-  Definition sepE (s : string) : list string  -> list string
-    := List.map (fun x => (x ++ s)%string).
-
-  Fixpoint unwords (ws : list string) : string :=
-  match ws with
-  | []  => ""
-  | [x] => x
-  | (x :: xs) => (x ++ " ") ++ unwords xs
-  end.
-
-End PrettyWords.
-
-
-Instance words_pretty : DOC Words
-  := { between  := PrettyWords.bet;
-       sepBy    := PrettyWords.sep;
-       sepEndBy := PrettyWords.sepE;
-       toString := Internal.intercalate " "
-     }.
-
-
-Module PrettyDoc.
-
-  Definition newline := String (ascii_of_nat 10) EmptyString.
-
-  Fixpoint spaces (n : nat) : string :=
-    match n with
-    | 0    => ""
-    | S m  => " " ++ spaces m
-    end.
-
-  Definition bet (b e : string) (doc : Doc) : Doc := [line [b]] ++ doc ++ [line [e]].
-
-
-  Definition endLineBy s (ln : Line) : Line :=
-    match ln with
-    | (n,ws) => (n, PrettyWords.endBy s ws)
-    end.
-  Fixpoint sep (s : string) (doc : Doc) : Doc :=
-    match doc with
-    | ln :: docp =>
-      match docp with
-      | [] => doc
-      | _  => endLineBy s ln :: sep s docp
-               end
-    | _  => doc
-    end.
-
-  Definition sepE (s : string) : Doc -> Doc
-    := List.map (endLineBy s).
-
-
-  Definition linetoStr (ln : Line) :=
-    match ln with
-    | (n,ws) => (spaces n ++ Internal.intercalate " " ws)%string
-    end.
-
-
-  Fixpoint unlines (doc : Doc) : string :=
-    Internal.intercalate newline (List.map linetoStr doc).
-
-
-End PrettyDoc.
-
-Instance doc_pretty : DOC Doc
-  := { between  := PrettyDoc.bet;
-       sepBy    := PrettyDoc.sep;
-       sepEndBy := PrettyDoc.sepE;
-       toString := PrettyDoc.unlines
-     }.
+Definition decimal n := text (Internal.nat_to_str n).
+Definition hexadecimal {n : nat} (u : Word.t (4 * n)) := Word.hex u.
