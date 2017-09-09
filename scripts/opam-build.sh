aad75df08990c1ea63cf61d6b24b037a22938015
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-09-09 13:21:14 +0530

    updated the opam build script.

--- scripts/opam-build.sh
+++ scripts/opam-build.sh
@@ -2,29 +2,77 @@
 
 COQ_BUILD_DIR="coq-$COQ_VER"
 
+if [ "$COQ_VER" == "" ]
+then
+    echo "Environment does not contain a setting for COQ_VER. Please set it up" 1>&2
+    exit 1;
+fi
+
+
+function setopamenv ()
+{
+    eval `opam config env --root="$COQ_BUILD_DIR"`
+}
+
 function init()
 {
     mkdir -p "coq-$COQ_VER"
     opam init --root="$COQ_BUILD_DIR" --no-setup --yes
-    eval `opam config env --root="$COQ_BUILD_DIR"`
-    coqtop -v
+    setopamenv
     opam install "coq.$COQ_VER" --yes
-    coq_makefile -f _CoqProject -o Makefile
+}
 
+function configure()
+{
+    init
+    setopamenv
+    ./configure.sh
 }
 
 function build()
 {
-    eval `opam config env --root="$COQ_BUILD_DIR"`
+
+    configure
+    setopamenv
     coqtop -v
     make
+}
+
+function html()
+{
+    configure
     make html
+}
+
+function install()
+{
+    build
+    make install
+}
+
+function usage()
+{
+
+cat <<EOF
+Usage : COQ_VER=[VERSION_OF_COQ] $0 [COMMAND]
+The set of allowed commands are
+
+  * init      - install necessary packages in an appropriate root.
+  * configure - get ready to make the project
+  * build     - build the project
+  * html      - build the html documentation
+  * install   - install the package in the current opam environment.
+
+EOF
 
 }
 
-
 case $1 in
     init) init; ;;
+    configure) configure; ;;
     build) build; ;;
-    *) echo "$0:error:" unknown command "$1" 
+    html) html; ;;
+    install) install; ;;
+    help) usage; ;;
+    *) echo "$0:error:" unknown command "$1" ; usage ; ;;
 esac
