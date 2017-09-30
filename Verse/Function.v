d4b878ff58bb6dd6ee9e16de297ac764943cb995
dangabhi <ahdang@iitk.ac.in>
2017-09-30 18:26:59 +0530

    Function records changed to work with Frames

--- Verse/Function.v
+++ Verse/Function.v
@@ -1,4 +1,4 @@
-Require Import Types.Internal.
+ Require Import Types.Internal.
 Require Import Syntax.
 Require Import Language.
 Require Import String.
@@ -19,13 +19,11 @@ Section Function.
                            cleanup : block v;
                          }.
 
-  Record FAllocation (pl lv rv : list type) (t : type) := fallocation
-                          {
-                            pa  : allocation v pl;
-                            lva : allocation v lv;
-                            lv  : v t;
-                            rva : allocation v rv;
-                          }.
+  Record FunVars   := funvars
+                         {
+                           param   : list type;
+                           local   : list type;
+                         }.
 
   Definition fscoped (t : type) (pl lv rv : list type) (T : Type) := scoped v pl (scoped v lv (scoped v (t :: nil) (scoped v rv T))).
 
@@ -35,11 +33,6 @@ Arguments name [v t] _ .
 Arguments setup [v t] _ .
 Arguments loop [v t] _ _ .
 Arguments cleanup [v t] _ .
-
-Arguments pa  [v pl lv rv t] _ .
-Arguments lva [v pl lv rv t] _ .
-Arguments lv  [v pl lv rv t] _ .
-Arguments rva [v pl lv rv t] _ .
 
 Definition func t pl lv rv := forall (v : varT),
                               scoped v pl (scoped v lv (scoped v rv (Function v t))).
