0dc45cb26ca7097523725189cfcb4a3ab9e4e4f4
dangabhi <ahdang@iitk.ac.in>
2017-10-03 11:42:27 +0530

    CCodeGen added

--- Verse/Function.v
+++ Verse/Function.v
@@ -6,33 +6,36 @@ Require Import Coq.Sets.Ensembles.
 Require Import List.
 Import ListNotations.
 
+Set Implicit Arguments.
 
 Section Function.
 
-  Variable v : varT.
+  Variable var : varT.
   
   Record Function   := function
                          {
-                           name    : string;
-                           setup   : block v;
-                           loop    : block v;
-                           cleanup : block v;
+                           setup   : block var;
+                           loop    : block var;
+                           cleanup : block var;
                          }.
 
   Record FunVars   := funvars
-                         {
-                           param   : list type;
-                           local   : list type;
-                         }.
+                        {
+                          fname   : string;
+                          param   : list type;
+                          local   : list type;
+                        }.
 
-  Definition fscoped (p l : list type) (T : Type) := scoped v p (scoped v l T).
+  Inductive userTy : varT :=
+  | inR ty : var ty -> userTy ty
+  | onS ty : userTy ty
+  .
+
+  Definition userAlloc (local : list type) := allocation userTy local.
 
 End Function.
 
-Arguments name [v] _ .
-Arguments setup [v] _ .
-Arguments loop [v] _ .
-Arguments cleanup [v] _ .
+Definition fscoped p l := forall (v : varT), scoped v p (scoped v l (Function v)).
 
-Definition func p l := forall (v : varT),
-                              scoped v p (scoped v l (Function v)).
+Definition func (reg : varT) (fv : FunVars) : Type := fscoped (param fv) (local fv) * userAlloc reg (local fv).
+
