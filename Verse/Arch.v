3990654ddb6e4b6125ba99bfc0f178ceeb015b9b
dangabhi <ahdang@iitk.ac.in>
2017-10-04 17:11:10 +0530

    complete FUNWRITE with opaque errors

--- Verse/Arch.v
+++ Verse/Arch.v
@@ -137,99 +137,69 @@ End CODEGEN.
 
 Module FUNWRITE (A : ARCH) (F : FRAME A) (C : CODEGEN A).
 
-  Definition TyListError (l : list type) := exists ty : type, List.In ty l /\ ~ A.supportedType ty.
-  Definition localAlloc (f : F.frameState) (ty : type) (ua : userTy A.register ty) : A.machineVar ty * F.frameState + { ~ A.supportedType ty \/ F.FrameError } :=
+  Inductive FunError : Prop :=
+  | InstructionNotSupported : instruction A.machineVar -> FunError
+  | NoLoopVariable          : FunError
+  | TypeNotSupported        : type -> FunError
+  | RegisterInUse (ty : type) : A.register ty -> FunError.
+
+  Definition localAlloc (f : F.frameState) (ty : type) (ua : userTy A.register ty) : A.machineVar ty * F.frameState + { FunError } :=
     match ua with
-    | inR _ _ r => F.useRegister f r
+    | inR _ ty r => match F.useRegister f r with
+                   | inleft x => inleft x
+                   | inright e => inright (match e with
+                                           | or_introl _ _ => TypeNotSupported ty
+                                           | or_intror _ _ => RegisterInUse r
+                                           end)
+                    end
     | onS _ ty  => match F.onStack f ty with
                    | inleft x => inleft x
-                   | inright e => inright (or_introl e)
+                   | inright _ => inright (TypeNotSupported ty)
                    end
     end.
   
-  Fixpoint pAlloc (p : list type) : forall (fr : F.frameState), F.frameState * allocation A.machineVar p + { TyListError p }.
-    refine (
+  Fixpoint pAlloc (p : list type) : forall (fr : F.frameState), F.frameState * allocation A.machineVar p + { FunError } :=
     fun (fr : F.frameState) => 
         match p with
         | [] => inleft (fr, emptyAllocation A.machineVar)
         | ty :: pt => match F.paramAlloc fr ty with
                       | inleft (vty, fr') => match pAlloc pt fr' with
                                              | inleft (fr'', a) => inleft (fr'', (vty, a))
-                                             | inright _        => inright _
+                                             | inright e        => inright e
                                              end
-                      | inright _         => inright _
+                      | inright _         => inright (TypeNotSupported ty)
                       end
-        end
-      ).
-    unfold TyListError. unfold TyListError in t.
-    inversion t.
-    inversion H.
-    refine (ex_intro _ x _ ).
-    constructor.
-    apply in_cons. all: trivial.
+        end.
 
-    unfold TyListError.
-    refine (ex_intro _ ty _).
-    constructor. apply in_eq. trivial.
-  Defined.
+  Fixpoint lAlloc (l : list type) : forall (la : userAlloc A.register l) (fr : F.frameState), F.frameState * allocation A.machineVar l + { FunError } :=
+    match l with
+    | []       => fun _ (fr : F.frameState) =>
+                    inleft (fr, emptyAllocation A.machineVar)
+    | ty :: lt => fun (la : userAlloc A.register (ty ::lt)) (fr : F.frameState) =>
+                    match localAlloc fr (fst la) with
+                    | inleft (vty, fr') => match lAlloc lt (snd la) fr' with
+                                           | inleft (fr'', a) => inleft (fr'', (vty, a))
+                                           | inright e        => inright e
+                                           end
+                    | inright e         => inright e
+                    end
+    end.
 
-  Fixpoint lAlloc (l : list type) : forall (la : userAlloc A.register l) (fr : F.frameState), F.frameState * allocation A.machineVar l + {TyListError l \/ F.FrameError}.
-    refine (
-          match l with
-          | []       => fun _ (fr : F.frameState) =>
-                          inleft (fr, emptyAllocation A.machineVar)
-          | ty :: lt => fun (la : userAlloc A.register (ty ::lt)) (fr : F.frameState) =>
-                          match localAlloc fr (fst la) with
-                          | inleft (vty, fr') => match lAlloc lt (snd la) fr' with
-                                                 | inleft (fr'', a) => inleft (fr'', (vty, a))
-                                                 | inright _        => inright _
-                                                 end
-                          | inright _         => inright _
-                          end
-          end
-      ).
-    inversion o.
-    refine (or_introl _ _).
-    unfold TyListError in H. unfold TyListError.
-    inversion H.
-    inversion H0.
-    refine (ex_intro _ x _).
-    constructor.
-    apply in_cons. all: trivial.
-    exact (or_intror _ H).
-
-    unfold TyListError.
-    inversion o.
-    refine (or_introl _ (ex_intro _ ty _)).
-    constructor. apply in_eq. trivial.
-    exact (or_intror _ H).
-  Defined.
 
   Arguments lAlloc [l].
 
-  Definition FunVarError (fv : FunVars) := TyListError (param fv) \/ TyListError (local fv).
-
-  Definition fFill (fv : FunVars) (f : func A.register fv) : F.frameState * Function A.machineVar + { FunVarError fv \/ F.FrameError }.
-    refine (
-        let ef := F.emptyFrame (fname fv) in
-        match pAlloc (param fv) ef with
-        | inleft (fr, pa) => match lAlloc (snd f) fr with
-                             | inleft (fr', la) => inleft (fr', fill la (fill pa (fst f A.machineVar)))
-                             | inright _        => inright _
-                             end
-        | inright _       => inright _
-        end
-    ).
-    inversion o.
-    refine (or_introl _ (or_intror _ H)).
-    refine (or_intror _ H).
-    refine (or_introl _ (or_introl _ t)).
-  Defined.
-
-  Inductive FunError : Prop :=
-  | InstructionNotSupported : instruction A.machineVar -> FunError
-  | NoLoopVariable          : FunError
-  | TypeNotSupported        : type -> FunError.
+  Definition fFill (fv : FunVars) (f : func A.register fv) : allocation A.machineVar (param fv) * F.frameState * Function A.machineVar + { FunError } :=
+    let ef := F.emptyFrame (fname fv) in
+(*    match pAlloc (param fv) ef with
+    | inleft (fr, pa) => match lAlloc (snd f) fr with
+                         | inleft (fr', la) => inleft (fr', fill la (fill pa (fst f A.machineVar)))
+                         | inright e        => inright e
+                         end
+    | inright e       => inright e
+    end.*)
+  x <- pAlloc (param fv) ef; let (fr, pa) := x in
+                             y <- lAlloc (snd f) fr; (let (fr', la) := y in
+                                                     inleft (pa, fr', fill la (fill pa (fst f A.machineVar)))).
 
   Fixpoint blockWrite (b : block A.machineVar) : Doc + { FunError } :=
         nest 4 <$>
@@ -244,20 +214,29 @@ Module FUNWRITE (A : ARCH) (F : FRAME A) (C : CODEGEN A).
                      end
         end.
 
-  Definition gen (fv : FunVars) (f : func A.register fv) : Doc + { FunError }.
-    refine (
-        match param fv with
-        | [] => inright NoLoopVariable
-        | _  =>
-          let fv' := {| param := A.Word :: param fv; local := local fv |} in
-          let f'  : func A.register fv' :=
-              ((fun v => @merge_scope _ [A.Word] _ _ (fun (n : v A.Word) => fst f v)), snd f) in
-          match fFill f' with
-          | inleft (fr, fn) => let fd := F.description fr in
-                               C.prologue fd <> blockWrite (setup fn) <> C.loopWrapper 
-          | inright _ => _
-          end
-        end
-      ).
+  Notation "A <>* B" := (ap (PrettyPrint.append A) B) (at level 70).
+  Notation "A *<> B" := (ap (fun x => PrettyPrint.append x B) A) (at level 70).
+  Notation "A *<>* B":= (apA (ap PrettyPrint.append A) B) (at level 70).
+
+  Definition gen (fv : FunVars) (f : func A.register fv) : Doc + { FunError } :=
+      let fv' := {| fname := fname fv; param := A.Word :: param fv; local := local fv |} in
+      let f'  : func A.register fv' :=
+          ((fun v => @merge_scope _ [A.Word] _ _ (fun (n : v A.Word) => fst f v)), snd f) in
+      match fFill f' with
+      | inleft (pa, fr, fn) => let fd := F.description fr in
+                               match param fv as p return forall (x : allocation A.machineVar (A.Word :: p)), _ with
+                               | [] => fun _ => inright NoLoopVariable
+                               | ty :: pt => 
+                                 fun x => match x with
+                                 | (count, (lv, pa')) =>
+                                   C.prologue fd <>* blockWrite (setup fn)
+                                              *<>* C.loopWrapper lv count <$> blockWrite (loop fn)
+                                              *<>* blockWrite (cleanup fn) *<> C.epilogue fd
+                                 end 
+                               end pa
+      | inright e => inright e
+      end
+  .
+
   
 End FUNWRITE.
