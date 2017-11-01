7b3052e3d331a9933f2084dea562e7b475682758
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-11-01 18:12:02 +0530

    updated the test code.

--- Verse/TestCode.v
+++ Verse/TestCode.v
@@ -1,6 +1,5 @@
 Require Import Verse.Error.
 Require Import Verse.Arch.C.
-Require Import Verse.Function.
 Require Import Verse.Types.
 Require Import Verse.Types.Internal.
 Require Import Verse.Language.
@@ -11,7 +10,12 @@ Require Import Verse.Word.
 Require Import String.
 Require Import List.
 Import ListNotations.
+(*
+Import ListNotations.
 
+Section TempCode.
+
+  Variable Word
 Definition loopvar := Byte.
 Definition pl      := [Ref (ArrayT 5 hostE Byte); Word16].
 Definition lv      := [ArrayT 5 hostE (ArrayT 3 hostE Word16); Word16; Word32].
@@ -33,51 +37,57 @@ Definition test : func C.register testVars :=
                loop  := [];
                cleanup := [];
              |}, lalloc).
-(*
-Definition test : func C.register testVars :=
-  (fun v : varT =>
-     fun seq : v (Ref (ArrayT 5 hostE Byte)) => 
-       fun num : v Word16 =>
-         fun arr : v (ArrayT 5 hostE (ArrayT 3 hostE Word16)) =>
-           fun (tmp : v Word16) (double : v Word32) =>
-             {|
-               (* Try out all operators *)
-               setup   := [
-                           num <= tmp [+] Ox "abcd";
-                             num <= tmp [-] num ; 
-                             num      <= tmp      [*] (arr[-1-])[-2-] ;
-                             num      <= (arr[-1-])[-2-] [/] tmp ;
-                             (arr[-1-]) <= tmp      [|] num ;
-                             num      <= tmp      [&] (arr[-1-])[-0-];
-                             num      <= tmp      [^] num ;
 
-                             (* binary update *)
-                             num <=+ tmp;
-                             num <=- (arr[-1-])[-1-];
-                             num <=* Ox "1234";
-                             num <=/ tmp;
-                             num <=| tmp;
-                             num <=& tmp;
-                             num <=^ tmp;
-
-                             (* Unary operators *)
-                             num      <=~ tmp;
-                             tmp      <=  (arr[-1-])[-1-] <<  42;
-                             tmp      <=  (arr[-1-])[-1-] >>  42;
-                             num      <=  tmp     <*< 42;
-                             (arr[-1-])[-1-] <=  tmp     >*> 42;
-
-                             (* Unary update operators *)
-                             tmp      <=<<  (42%nat);
-                             tmp      <=>>  (42%nat);
-                             num      <=<*< (42%nat);
-                             (arr[-1-])[-1-] <=>*> (42%nat)
-                         ]%list;
-               loop    := [num <=  tmp [+] (arr[-1-])[-1-] ];
-               cleanup := []
-             |}, lalloc).
 *)
-Definition code := CFunWrite.gen test.
+
+
+Section TestFunction.
+
+  Variable variable : varT.
+  (* The parameters of the function *)
+  Variable arr     : variable (ArrayT 5 hostE Word16).
+  Variable A B     : variable Byte.
+  (* The local variables *)
+  Variable num      : variable Word16.
+  (* The temp register *)
+  Variable tmp       : variable Word16.
+
+  Definition testFunction : list (instruction variable) :=
+    [ num <= tmp [+] Ox "abcd";
+      A   <= A [+] B;
+      num <= tmp [-] num ;
+      num      <= tmp      [*] arr[-1-];
+      num      <= arr[-1-] [/] tmp ;
+      arr[-1-] <= tmp      [|] num ;
+      num      <= tmp      [&] arr[-1-];
+      num      <= tmp      [^] num ;
+      (* binary update *)
+      num <=+ tmp;
+      num <=- arr[-1-];
+      num <=* Ox "1234";
+      num <=/ tmp;
+      num <=| tmp;
+      num <=& tmp;
+      num <=^ tmp;
+      
+      (* Unary operators *)
+      num      <=~ tmp;
+      tmp      <=  arr[-1-] <<  42;
+      tmp      <=  arr[-1-] >>  42;
+      num      <=  tmp     <*< 42;
+      arr[-1-] <=  tmp     >*> 42;
+      
+      (* Unary update operators *)
+      tmp      <=<<  (42%nat);
+      tmp      <=>>  (42%nat);
+      num      <=<*< (42%nat);
+      arr[-1-] <=>*> (42%nat)
+    ].
+      
+End TestFunction.
+
+Definition regVars : allocation C.register [Word16] := (cr Word16 "temp", tt). 
+Definition code := CompileC.compile "testFunction" [ArrayT 5 hostE Word16; Byte; Byte] [Word16] regVars testFunction.
 
 Compute (recover (layout <$> code)).
 
