692761eebd59e9e2dc6416a83438e08b44ad813b
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-12-19 18:37:51 +0530

    add a top level module that expose all required stuff.

---
+++ Verse.v
@@ -0,0 +1,11 @@
+Require Export Verse.Language.
+Require Export Verse.Types.
+Require Export Verse.Types.Internal.
+Require Export Verse.Word.
+Require Export Verse.Error.
+Require Export Verse.Syntax.
+Require Export Verse.PrettyPrint.
+Require Export String.
+Require Export List.
+Require Export Vector.
+Export ListNotations.
