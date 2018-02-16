6a6b919bf2a9f21492f6376a62b0d6f4b787ba2a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-02-16 18:03:04 +0530

    update the C architecture to use the new move instruction.

--- src/Verse/Arch/C.v
+++ src/Verse/Arch/C.v
@@ -88,24 +88,36 @@ Section PrintingInstruction.
                                             | _      => text "Unsupported"
                                             end.
 
-  Definition toLE ty v := text ("verse_to_le") <> wordSize ty <> paren v.
-  Definition toBE ty v := text ("verse_to_be") <> wordSize ty <> paren v.
-  Definition fromLE ty v := text ("verse_from_le") <> wordSize ty <> paren v.
-  Definition fromBE ty v := text ("verse_from_be") <> wordSize ty <> paren v.
+  Definition toMemory e ty v :=
+   match e with
+   | littleE => text "verse_to_le" <> wordSize ty <> (paren v)
+   | bigE    => text "verse_to_be" <> wordSize ty <> (paren v)
+   | _       => v
+   end.
+
+  Definition fromMemory e ty v :=
+   match e with
+   | littleE => text "verse_from_le" <> wordSize ty <> (paren v)
+   | bigE    => text "verse_form_be" <> wordSize ty <> (paren v)
+   | _       => v
+   end.
 
   Definition rval {k} {ty : type k} (a : arg cvar _ ty) :=
     match a with
-    | @Language.index _ _ littleE ty _ _ => fromLE ty (doc a)
-    | @Language.index _ _ bigE ty _ _    => fromBE ty (doc a)
-    | _                                  => doc a
+    | @Language.index _ _ e ty _ _ => fromMemory e ty (doc a)
+    | _                            => doc a
     end.
 
+
+
   Definition CAssign {a : arity} (o : op a) {k} {ty : type k} (a : arg cvar _ ty) (y z : Doc)  :=
+    let lhs := y <_> opDoc o <_> z
+    in
     match a with
-    | @Language.index _ _ littleE ty _ _ => doc a <_> EQUALS <_> toLE ty (y <_> opDoc o <_> z)
-    | @Language.index _ _ bigE ty _ _ => doc a <_> EQUALS <_> toBE ty (y <_> opDoc o <_> z)
-    | _                                  => doc a <_> EQUALS <_> y <_> opDoc o <_> z
+    | @Language.index _ _ e ty _ _ => CP.assign (doc a) (toMemory e ty lhs)
+    | _                            => CP.assign (doc a) lhs
     end.
+
 
   Definition CRot (ty : type direct) (o : op unary) (a : arg cvar _ ty) (y : Doc)  :=
     let rvl := match o with
@@ -115,16 +127,14 @@ Section PrintingInstruction.
                end
     in
     match a with
-    | @Language.index _ _ littleE ty _ _ => doc a <_> EQUALS <_> toLE ty rvl
-    | @Language.index _ _ bigE ty _ _    => doc a <_> EQUALS <_> toBE ty rvl
-    | _                                  => doc a <_> EQUALS <_> rvl
+    | @Language.index _ _ e ty _ _ => CP.assign (doc a) (toMemory e ty rvl)
+    | _                            => CP.assign (doc a) rvl
     end.
 
   Definition CUpdate {a : arity}(o : op a) {k} {ty : type k} (a : arg cvar _ ty) (y : Doc) :=
     match a with
-    | @Language.index _ _ littleE ty _ _ => CAssign o a (rval a) y
-    | @Language.index _ _ bigE ty _ _    => CAssign o a (rval a) y
-    | _                                  => doc a <_> opDoc o <> EQUALS <_> y
+    | Language.index _ _  => CAssign o a (rval a) y
+    | _                   => doc a <_> opDoc o <> EQUALS <_> y
     end.
 
   Global Instance assignment_C_print : PrettyPrint (assignment cvar) | 0
@@ -133,13 +143,13 @@ Section PrintingInstruction.
                               | update2 o x y   => CUpdate o x (rval y)
                               | @assign2 _ ty u x y   =>
                                 match u with
-                                | bitComp  | nop | mov => CAssign u x empty (rval y)
+                                | bitComp  | nop => CAssign u x empty (rval y)
                                 | shiftL n | shiftR n  => CAssign u x (rval y) (decimal n)
                                 | rotL n   | rotR n    => CRot u x (rval y)
                                 end
                               | @update1 _ ty u x      =>
                                 match u with
-                                | bitComp  | nop | mov => CAssign u x empty (rval x)
+                                | bitComp  | nop => CAssign u x empty (rval x)
                                 | shiftL n | shiftR n  => CUpdate u x (decimal n)
                                 | rotL n   | rotR n    => CRot u x (rval x)
                                 end
@@ -149,6 +159,7 @@ Section PrintingInstruction.
   Global Instance instruction_C_print : PrettyPrint (instruction cvar) | 0
     := { doc := fun i => match i with
                          | assign a => doc a
+                         | moveTo a ix b => CAssign nop (Language.index a ix) empty (rval (toArg b))
                          end
        }.
 
@@ -168,7 +179,7 @@ Module C <: ARCH.
   Definition machineVar := cvar.
 
   Definition HostEndian := hostE.
-  
+
   Definition Word := Word64.
 
   Definition embedRegister := inRegister.
