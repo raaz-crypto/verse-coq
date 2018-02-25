950f5e1c374fb15a186a1a6833f17ab4c3e35bcf
dangabhi <ahdang@iitk.ac.in>
2018-02-25 12:50:11 +0530

    DecFacts rewritten to work with argKind

--- src/Verse/DecFacts.v
+++ src/Verse/DecFacts.v
@@ -10,202 +10,12 @@ Require Import Equality.
 Require Vector.
 Require Import VectorEq.
 
-(** Porting over Coq.decidable for decidability with Type *)
 
 Definition decidable (P:Prop) := {P} + {~ P}.
 
 Definition eq_dec (T : Type) := forall (t1 t2 : T),  decidable (t1 = t2).
 
 Definition nat_eq_dec := NPeano.Nat.eq_dec.
-
-Theorem dec_not_not : forall P:Prop, decidable P -> (~ P -> False) -> P.
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem dec_True : decidable True.
-Proof.
-unfold decidable; auto.
-Qed.
-
-Theorem dec_False : decidable False.
-Proof.
-unfold decidable, not; auto.
-Qed.
-
-Theorem dec_or :
- forall A B:Prop, decidable A -> decidable B -> decidable (A \/ B).
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem dec_and :
- forall A B:Prop, decidable A -> decidable B -> decidable (A /\ B).
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem dec_not : forall A:Prop, decidable A -> decidable (~ A).
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem dec_imp :
- forall A B:Prop, decidable A -> decidable B -> decidable (A -> B).
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem dec_iff :
- forall A B:Prop, decidable A -> decidable B -> decidable (A<->B).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-Theorem not_not : forall P:Prop, decidable P -> ~ ~ P -> P.
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem not_or : forall A B:Prop, ~ (A \/ B) -> ~ A /\ ~ B.
-Proof.
-tauto.
-Qed.
-
-Theorem not_and : forall A B:Prop, decidable A -> ~ (A /\ B) -> ~ A \/ ~ B.
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem not_imp : forall A B:Prop, decidable A -> ~ (A -> B) -> A /\ ~ B.
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem imp_simp : forall A B:Prop, decidable A -> (A -> B) -> ~ A \/ B.
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem not_iff :
-  forall A B:Prop, decidable A -> decidable B ->
-    ~ (A <-> B) -> (A /\ ~ B) \/ (~ A /\ B).
-Proof.
-unfold decidable; tauto.
-Qed.
-
-(** Results formulated with iff, used in FSetDecide.
-    Negation are expanded since it is unclear whether setoid rewrite
-    will always perform conversion. *)
-
-(** We begin with lemmas that, when read from left to right,
-    can be understood as ways to eliminate uses of [not]. *)
-
-Theorem not_true_iff : (True -> False) <-> False.
-Proof.
-tauto.
-Qed.
-
-Theorem not_false_iff : (False -> False) <-> True.
-Proof.
-tauto.
-Qed.
-
-Theorem not_not_iff : forall A:Prop, decidable A ->
-  (((A -> False) -> False) <-> A).
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Theorem contrapositive : forall A B:Prop, decidable A ->
-  (((A -> False) -> (B -> False)) <-> (B -> A)).
-Proof.
-unfold decidable; tauto.
-Qed.
-
-Lemma or_not_l_iff_1 : forall A B: Prop, decidable A ->
-  ((A -> False) \/ B <-> (A -> B)).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-Lemma or_not_l_iff_2 : forall A B: Prop, decidable B ->
-  ((A -> False) \/ B <-> (A -> B)).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-Lemma or_not_r_iff_1 : forall A B: Prop, decidable A ->
-  (A \/ (B -> False) <-> (B -> A)).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-Lemma or_not_r_iff_2 : forall A B: Prop, decidable B ->
-  (A \/ (B -> False) <-> (B -> A)).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-Lemma imp_not_l : forall A B: Prop, decidable A ->
-  (((A -> False) -> B) <-> (A \/ B)).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-
-(** Moving Negations Around:
-    We have four lemmas that, when read from left to right,
-    describe how to push negations toward the leaves of a
-    proposition and, when read from right to left, describe
-    how to pull negations toward the top of a proposition. *)
-
-Theorem not_or_iff : forall A B:Prop,
-  (A \/ B -> False) <-> (A -> False) /\ (B -> False).
-Proof.
-tauto.
-Qed.
-
-Lemma not_and_iff : forall A B:Prop,
-  (A /\ B -> False) <-> (A -> B -> False).
-Proof.
-tauto.
-Qed.
-
-Lemma not_imp_iff : forall A B:Prop, decidable A ->
-  (((A -> B) -> False) <-> A /\ (B -> False)).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-Lemma not_imp_rev_iff : forall A B : Prop, decidable A ->
-  (((A -> B) -> False) <-> (B -> False) /\ A).
-Proof.
-unfold decidable. tauto.
-Qed.
-
-(** With the following hint database, we can leverage [auto] to check
-    decidability of propositions. *)
-
-Hint Resolve dec_True dec_False dec_or dec_and dec_imp dec_not dec_iff
- : decidable_prop.
-
-(** [solve_decidable using lib] will solve goals about the
-    decidability of a proposition, assisted by an auxiliary
-    database of lemmas.  The database is intended to contain
-    lemmas stating the decidability of base propositions,
-    (e.g., the decidability of equality on a particular
-    inductive type). *)
-
-Tactic Notation "solve_decidable" "using" ident(db) :=
-  match goal with
-   | |- decidable _ =>
-     solve [ auto 100 with decidable_prop db ]
-  end.
-
-Tactic Notation "solve_decidable" :=
-  solve_decidable using core.
-
 
 Section DecFacts.
 
@@ -251,11 +61,15 @@ End DecFacts.
 
 Scheme Equality for kind.
 
-Lemma endian_eq_dec : forall (e1 e2 : endian), {e1 = e2} + {e1 <> e2}.
+Lemma endian_eq_dec : eq_dec endian.
+  unfold eq_dec; unfold decidable.
   decide equality.
 Qed.
 
-Lemma ty_eq_dec : forall {k} (t1 t2 : type k), {t1 = t2} + {t1 <> t2}.
+Scheme Equality for align.
+
+Lemma ty_eq_dec : forall {k}, eq_dec (type k).
+  unfold eq_dec; unfold decidable.
   destruct k.
 
   abstract (dependent destruction t1; dependent destruction t2; [try (right; congruence) ..];
@@ -264,12 +78,13 @@ Lemma ty_eq_dec : forall {k} (t1 t2 : type k), {t1 = t2} + {t1 <> t2}.
   using directTy_eq_dec.
 
   dependent destruction t1; dependent destruction t2.
-  destruct (nat_eq_dec n n0); destruct (endian_eq_dec e e0); destruct (directTy_eq_dec t1 t2);
+  destruct (align_eq_dec a a0); destruct (nat_eq_dec n n0); destruct (endian_eq_dec e e0); destruct (directTy_eq_dec t1 t2);
     first [ left; congruence | right; congruence ].
 Qed.
 
-Lemma bytes_dec : forall (n : nat) (a b : bytes n), {a = b} + {a <> b}.
-  unfold bytes. destruct a. destruct b0.
+Lemma bytes_dec : forall (n : nat), eq_dec (bytes n).
+  unfold eq_dec; unfold decidable.
+  unfold bytes. destruct t1. destruct t2.
   unfold Bvector.Bvector in b, b0.
   pose (vec_eq_dec bool bool_dec (8 * n) b b0) as s.
   destruct s.
@@ -277,58 +92,33 @@ Lemma bytes_dec : forall (n : nat) (a b : bytes n), {a = b} + {a <> b}.
   right; unfold not; inversion 1; contradiction.
 Qed.
 
-Lemma constant_dec {k} (ty : type k) : forall (a b : constant ty), {a = b} + {a <> b}.
-  induction ty;
+Lemma constant_dec ty : eq_dec (constant ty).
+  unfold eq_dec; unfold decidable.
+  dependent induction ty;
   simpl; try apply vec_eq_dec; try apply bytes_dec; trivial.
 Qed.
 
 Section LanguageDecs.
 
   Variable v : VariableT.
-  Variable v_eq_dec : forall {k} {ty : type k} (v1 v2 : v _ ty), {v1 = v2} + {v1 <> v2}.
+  Variable v_eq_dec : forall {k} {ty : type k}, eq_dec (v _ ty).
 
   Arguments v_eq_dec [k ty] _.
 
-  Lemma op_eq_dec {a} (o1 o2 : op a) : {o1 = o2} + {o1 <> o2}.
-    destruct a; dependent destruction o1; dependent destruction o2;
+  Lemma op_eq_dec {a} : eq_dec (op a).
+    unfold eq_dec; unfold decidable.
+    destruct a; dependent destruction t1; dependent destruction t2;
       solve [left; congruence |
              right; congruence |
              destruct (nat_eq_dec n n0); solve [left; congruence | right; congruence]
             ].
   Qed.
 
+  Scheme Equality for argKind.
 
-  (*
-  Lemma arg_eq_dec : forall (a1 a2 : {ty : type direct & arg _ ty}), {a1 = a2} + {a1 <> a2}.
-    destruct a1, a2.
-    dependent destruction a; dependent destruction a0;
-      try (solve [(right; unfold not; inversion 1)]).
-    * destruct (ty_eq_dec x x0); subst; [ | right ; unfold not; inversion 1; contradiction ..].
-      destruct (v_eq_dec v0 v1); [left; congruence | right];
-        unfold not; inversion 1.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H1); contradiction.
-    * destruct (ty_eq_dec x x0); subst; [ | right ; unfold not; inversion 1; contradiction ..].
-      destruct (constant_dec x0 c c0); [left; congruence | right];
-        unfold not; inversion 1.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H1).
-      contradiction.
-    * destruct (ty_eq_dec x x0); destruct (eq_dec b b0);
-        destruct (endian_eq_dec e e0); destruct (ty_eq_dec x x0);
-          subst; [ | right; unfold not; inversion 1; contradiction ..].
-      destruct (v_eq_dec v0 v1).
-      destruct (exists_eq_dec _ eq_dec _ (fun n => lt_eq_dec n b0) s s0);
-        [ left; congruence | right ].
-      unfold not; inversion 1.
-      pose (inj_pair2_eq_dec _ eq_dec _ _ _ _ H2); contradiction.
-      right; unfold not; inversion 1.
-      pose (inj_pair2_eq_dec _ eq_dec _ _ _ _ H1).
-      pose (inj_pair2_eq_dec _ endian_eq_dec _ _ _ _ e).
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ e1); contradiction.
-  Defined.
-   *)
-
-  Lemma arg_eq_dec {ty : type direct} : forall (a1 a2 : arg v _ ty), {a1 = a2} + {a1 <> a2}.
-    dependent destruction a1; dependent destruction a2;
+  Lemma arg_eq_dec {ty : type direct} {aK} : eq_dec (arg v aK _ ty).
+    unfold eq_dec; unfold decidable.
+    dependent destruction t1; dependent destruction t2;
       try (solve [(right; unfold not; inversion 1)]).
     * destruct (v_eq_dec v0 v1); [left; congruence | right];
         unfold not; inversion 1.
@@ -337,49 +127,80 @@ Section LanguageDecs.
         unfold not; inversion 1.
       pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H1).
       contradiction.
-    * destruct (nat_eq_dec b b0);
+    * destruct (align_eq_dec a a0);
+        destruct (nat_eq_dec b b0);
         destruct (endian_eq_dec e e0);
         subst; [ | right; unfold not; inversion 1; contradiction ..].
-      destruct (v_eq_dec v0 v1).
-      destruct (exists_eq_dec _ nat_eq_dec _ (fun n => lt_eq_dec n b0) s s0);
+      destruct (v_eq_dec x x0); subst; [ | right; unfold not; inversion 1].
+      destruct (exists_eq_dec _ nat_eq_dec _ (fun n => lt_eq_dec n b0) i i0);
         [ left; congruence | right ].
       unfold not; inversion 1.
-      pose (inj_pair2_eq_dec _ nat_eq_dec _ _ _ _ H2); contradiction.
-      right; unfold not; inversion 1.
+      pose (inj_pair2_eq_dec _ nat_eq_dec _ _ _ _ H1).
+      pose (inj_pair2_eq_dec _ endian_eq_dec _ _ _ _ e).
+      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ e1).
+      pose (inj_pair2_eq_dec _ (@v_eq_dec memory (array a0 b0 e0 ty)) _ _ _ _ e2); contradiction.
+
       pose (inj_pair2_eq_dec _ nat_eq_dec _ _ _ _ H1).
       pose (inj_pair2_eq_dec _ endian_eq_dec _ _ _ _ e).
       pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ e1); contradiction.
   Defined.
 
-  Definition instruction_eq_dec (i1 i2 : instruction v) : {i1 = i2} + {i1 <> i2}.
-    destruct i1 as [a1]; destruct i2 as [a2].
-    dependent destruction a1; dependent destruction a2; try (solve [right; inversion 1]);
-      destruct (ty_eq_dec ty ty0); try (solve [right; inversion 1; contradiction]); subst.
-    - destruct (op_eq_dec b b0); try (solve [right; inversion 1; contradiction]).
-      destruct (arg_eq_dec a a2); destruct (arg_eq_dec a0 a3); destruct (arg_eq_dec a1 a4);
-        [left; congruence | right; inversion 1 ..].
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H4); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H4); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H4); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
-    - destruct (op_eq_dec u u0); try (solve [right; inversion 1; contradiction]).
-      destruct (arg_eq_dec a a1); destruct (arg_eq_dec a0 a2);
-        [left; congruence | right; inversion 1 ..].
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
-    - destruct (op_eq_dec b b0); try (solve [right; inversion 1; contradiction]).
-      destruct (arg_eq_dec a a1); destruct (arg_eq_dec a0 a2);
-        [left; congruence | right; inversion 1 ..].
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
-    - destruct (op_eq_dec u u0); try (solve [right; inversion 1; contradiction]).
-      destruct (arg_eq_dec a a0); [left; congruence | right; inversion 1 ..].
-      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+  Definition instruction_eq_dec : eq_dec (instruction v).
+    unfold eq_dec; unfold decidable.
+    destruct t1 as [a1 | m1 | d1]; destruct t2 as [a2 | m2 | d2]; try (solve [right; inversion 1]).
+    - dependent destruction a1; dependent destruction a2; try (solve [right; inversion 1]);
+        destruct (ty_eq_dec ty ty0); try (solve [right; inversion 1; contradiction]); subst.
+      * destruct (op_eq_dec b b0); try (solve [right; inversion 1; contradiction]).
+        destruct (arg_eq_dec l l0); destruct (arg_eq_dec r r1); destruct (arg_eq_dec r0 r2);
+          [left; congruence | right; inversion 1 ..].
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H4); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H4); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H4); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+      * destruct (op_eq_dec u u0); try (solve [right; inversion 1; contradiction]).
+        destruct (arg_eq_dec l l0); destruct (arg_eq_dec r r0);
+          [left; congruence | right; inversion 1 ..].
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+      * destruct (op_eq_dec b b0); try (solve [right; inversion 1; contradiction]).
+        destruct (arg_eq_dec l l0); destruct (arg_eq_dec r r0);
+          [left; congruence | right; inversion 1 ..].
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+      * destruct (op_eq_dec u u0); try (solve [right; inversion 1; contradiction]).
+        destruct (arg_eq_dec l l0); [left; congruence | right; inversion 1 ..].
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+    - destruct (align_eq_dec m1 m2);
+        destruct (nat_eq_dec b b0);
+        destruct (endian_eq_dec e e0);
+        destruct (ty_eq_dec ty ty0);
+        try (solve [right; inversion 1; contradiction]); subst.
+      destruct (v_eq_dec x x0); [ subst | right; inversion 1];
+        destruct (exists_eq_dec _ nat_eq_dec _ (fun n => lt_eq_dec n b0) i i0);
+        destruct (v_eq_dec v0 v1); [left; congruence | subst; try (right; inversion 1) ..].
+      * pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H1); contradiction.
+      * pose (inj_pair2_eq_dec _ nat_eq_dec _ _ _ _ H1).
+        pose (inj_pair2_eq_dec _ endian_eq_dec _ _ _ _ e).
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ e1).
+        pose (inj_pair2_eq_dec _ (@v_eq_dec _ (array m2 b0 e0 ty0)) _ _ _ _ e2); contradiction.
+      * pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H2); contradiction.
+      * pose (inj_pair2_eq_dec _ nat_eq_dec _ _ _ _ H1).
+        pose (inj_pair2_eq_dec _ endian_eq_dec _ _ _ _ e).
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ e1); contradiction.
+      * pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
+      * pose (inj_pair2_eq_dec _ nat_eq_dec _ _ _ _ H1).
+        pose (inj_pair2_eq_dec _ endian_eq_dec _ _ _ _ e).
+        pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ e1); contradiction.
+      * pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H3); contradiction.
+    - destruct (kind_eq_dec d1 d2); [ subst | right; inversion 1; contradiction].
+      destruct (ty_eq_dec ty ty0); [ subst | right; inversion 1; contradiction].
+      destruct (v_eq_dec v0 v1); [left; congruence | right; inversion 1].
+      pose (inj_pair2_eq_dec _ ty_eq_dec _ _ _ _ H1); contradiction.
   Defined.
 
 End LanguageDecs.
