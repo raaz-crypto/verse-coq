40688028c358b9f40aae850e14f3964e4c12d712
Abhishek Dang <ahdang@iitk.ac.in>
2018-10-16 00:22:27 +0530

    StandardSemantics added

---
+++ src/Verse/Semantics/StandardSemantics.v
@@ -0,0 +1,6 @@
+Require Import Verse.Types.Internal.
+Require Import Verse.Types.
+Require Import Verse.Language.Operators.
+Require Import Verse.Semantics.
+
+Module StandardSemantics := CodeSemantics StandardWord StandardConsts StandardWordOps.
