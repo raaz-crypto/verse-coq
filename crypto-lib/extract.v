5fcfd7f68c20155ab85f0207f273db6a05841e18
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-10-18 19:33:20 +0530

    got rid of m4 based extract.v generation.

---
+++ crypto-lib/extract.v
@@ -0,0 +1,7 @@
+Require Verse.CryptoLib.chacha20.c.portable.
+Require Verse.CryptoLib.sha256.c.portable.
+Require Verse.CryptoLib.sha512.c.portable.
+
+Extraction "mlsrc//chacha20/c/portable" Verse.CryptoLib.chacha20.c.portable.implementation.
+Extraction "mlsrc//sha256/c/portable"   Verse.CryptoLib.sha256.c.portable.implementation.
+Extraction "mlsrc//sha512/c/portable"   Verse.CryptoLib.sha512.c.portable.implementation.
