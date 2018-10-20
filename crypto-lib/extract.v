b2cd7c31bb70a81c08578c3fd9990f477792fbb1
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-10-20 21:13:58 +0530

    extraction handles the raaz module generation

--- crypto-lib/extract.v
+++ crypto-lib/extract.v
@@ -2,6 +2,10 @@ Require Verse.CryptoLib.chacha20.c.portable.
 Require Verse.CryptoLib.sha256.c.portable.
 Require Verse.CryptoLib.sha512.c.portable.
 
-Extraction "mlsrc//chacha20/c/portable" Verse.CryptoLib.chacha20.c.portable.implementation.
-Extraction "mlsrc//sha256/c/portable"   Verse.CryptoLib.sha256.c.portable.implementation.
-Extraction "mlsrc//sha512/c/portable"   Verse.CryptoLib.sha512.c.portable.implementation.
+Extraction "mlsrc/chacha20/c/portable" Verse.CryptoLib.chacha20.c.portable.implementation.
+Extraction "mlsrc/sha256/c/portable"   Verse.CryptoLib.sha256.c.portable.implementation.
+Extraction "mlsrc/sha512/c/portable"   Verse.CryptoLib.sha512.c.portable.implementation.
+
+Extraction "mlsrc/raaz/chacha20/c/portable" Verse.CryptoLib.chacha20.c.portable.raazFFI.
+Extraction "mlsrc/raaz/sha256/c/portable"   Verse.CryptoLib.sha256.c.portable.raazFFI.
+Extraction "mlsrc/raaz/sha512/c/portable"   Verse.CryptoLib.sha512.c.portable.raazFFI.
