60709ec348ce550163388c77bc5de0da2ed6a583
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-10-26 12:01:43 +0530

    blake2 config module.

---
+++ src/Verse/CryptoLib/blake2.v
@@ -0,0 +1,31 @@
+Require Import Verse.
+Require Vector.
+Import VectorNotations.
+Delimit Scope vector_scope with vector.
+
+Module Type CONFIG.
+
+  (** The word size for the hash *)
+  Parameter WORD_LOG_SIZE : nat.
+
+  (** The number of rounds that is involved in hashing. *)
+  Parameters ROUNDS : nat.
+
+  (** The round constants for the hash *)
+  Parameter IV : Vector.t (constant (word WORD_LOG_SIZE)) 8.
+
+  (** The rotation constants used by the G function *)
+  Parameter R0 R1 R2 R3 : nat.
+
+
+End CONFIG.
+
+(**
+
+A single block of both sha512 and sha256 are 16-words in big endian
+and the hash value is 8-words long.
+
+*)
+
+Definition HASH_SIZE : nat := 8.
+Definition BLOCK_SIZE : nat := 16.
