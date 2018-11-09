a5d1960157ed5e985674b652674b0ffd5129da6c
dangabhi <ahdang@iitk.ac.in>
2018-11-09 21:17:12 +0530

    Semantics now parametrized on a 'store' type

--- src/Verse/Semantics.v
+++ src/Verse/Semantics.v
@@ -5,6 +5,7 @@ Require Import Verse.Syntax.
 Require Import Verse.Error.
 Require Import Verse.Word.
 Require Import Verse.DecFacts.
+Require Import Verse.Semantics.Store.
 
 Require Import Bvector.
 Require Import Vector.
@@ -25,47 +26,38 @@ Module Semantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS
 
   Section Semantics.
 
-    Variable v : VariableT.
-    Variable v_eqb : forall {k} {ty : type k} (v1 v2 : v ty), bool.
+    Variable s : Store.
 
-    Definition contextUpdate k {ty : type k} (var : v ty)
-               (f : @typeDenote TypeDenote _ _ ty -> @typeDenote TypeDenote _ _ ty) :
-      context v -> context v.
-      unfold context in *. intros oldctxt k0 ty0 x.
-      destruct (kind_eq_dec k k0); subst.
-      destruct (ty_eq_dec ty ty0); subst.
-      destruct (v_eqb x var); subst.
-      exact (f (oldctxt _ _ var)).               (* update according to f at var *)
-      all: exact (oldctxt _ _ x).                (* use initial state value at all other points *)
-    Defined.
-
-    Arguments contextUpdate [k ty] _ _ _.
+    Let store := store s.
+    Let v := v s.
+    Let eval := eval s.
+    Let storeUpdate := storeUpdate s.
 
     Section InstructionDenote.
 
       (* Extracts from the context v the value denoted by an arg *)
 
-      Definition argDenote (S : context v)
+      Definition argDenote (S : store)
                  `{ty : type k} `(a : arg v aK ty)
       : @typeDenote TypeDenote _ _ ty :=
         match a in (arg _ _ ty) return typeDenote ty with
-        | var av               => S _ _ av
+        | var av               => eval S _ av
         | Language.Ast.const c => C.constDenote c
-        | index x i            => (fun y => nth_order y (proj2_sig i)) (S _ _ x)
+        | index x i            => (fun y => nth_order y (proj2_sig i)) (eval S _ x)
         end.
 
-      (* Auxiliary function to lift an arg value change to context v *)
+      (* Auxiliary function to lift an arg value change to store *)
       Definition largUpdate `{ty : type k} (a : larg v ty)
                  (val : typeDenote ty : Type)
-                 (S : context v)
-        : context v :=
+                 (S : store)
+        : store :=
         match a in arg _ lval ty  return (typeDenote ty : Type)
-                                         -> context v
+                                         -> store
         with
-        | @var _ lval _ _ av        => fun val' => contextUpdate av
+        | @var _ lval _ _ av        => fun val' => storeUpdate _ av
                                                                (fun _ => val')
                                                                S
-        | @index _ lval  _ _ _ x i => fun val' => contextUpdate x
+        | @index _ lval  _ _ _ x i => fun val' => storeUpdate _ x
                                                               (fun vec =>
                                                                  replace_order vec (proj2_sig i) val')
                                                               S
@@ -74,55 +66,55 @@ Module Semantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS
       (* The constant 1 as a verse constant to provide an interpretation
          for the increment/decrement instructions
        *)
-    Let one (ty : type direct) : constant ty :=
-      match ty as ty' in type direct return constant ty' with
-      | word n        => Nibble.Internal.fromNR _ 1
-      | multiword _ _ => const (Nibble.Internal.fromNR _ 1) _
-      end
-    .
+      Let one (ty : type direct) : constant ty :=
+        match ty as ty' in type direct return constant ty' with
+        | word n        => Nibble.Internal.fromNR _ 1
+        | multiword _ _ => const (Nibble.Internal.fromNR _ 1) _
+        end
+      .
 
-    Fixpoint  instructionDenote (i : instruction v) (S : context v) {struct i}
-      : (context v) :=
-      let updatePair `{ty : type k} (a1 a2 : larg v ty) val :=
-          let S' := largUpdate a1 (fst val) S in largUpdate a2 (snd val) S' in
-      match i with
-      | increment la => largUpdate la (OP.opDenote _ plus
-                                                   (argDenote S la)
-                                                   (argDenote S (Ast.const (one _))))
-                                   S
-      | decrement la => largUpdate la (OP.opDenote _ minus
-                                                   (argDenote S la)
-                                                   (argDenote S (Ast.const (one _))))
-                                   S
-      | assign ass => match ass with
-                      | extassign4 op la1 la2 ra1 ra2 ra3 =>
-                        updatePair la1 la2 (OP.opDenote _ op
-                                                        (argDenote S ra1)
-                                                        (argDenote S ra2)
-                                                        (argDenote S ra3))
-                      | extassign3 op la1 la2 ra1 ra2     =>
-                        updatePair la1 la2 (OP.opDenote _ op
-                                                        (argDenote S ra1)
-                                                        (argDenote S ra2))
-                      | assign3 op la ra1 ra2 => largUpdate la (OP.opDenote _ op
-                                                                            (argDenote S ra1)
-                                                                            (argDenote S ra2))
-                                                            S
-                      | assign2 op la ra1     => largUpdate la (OP.opDenote _ op
-                                                                            (argDenote S ra1))
-                                                            S
-                      | update2 op la ra1     => largUpdate la (OP.opDenote _ op
-                                                                            (argDenote S la)
-                                                                            (argDenote S ra1))
-                                                            S
-                      | update1 op la         => largUpdate la (OP.opDenote _ op
-                                                                            (argDenote S la))
-                                                            S
-                      end
-      | moveTo x ix ra => largUpdate (index x ix) (@argDenote S _ _ rval (var ra))
+      Fixpoint  instructionDenote (i : instruction v) (S : store) {struct i}
+        : (store) :=
+        let updatePair `{ty : type k} (a1 a2 : larg v ty) val :=
+            let S' := largUpdate a1 (fst val) S in largUpdate a2 (snd val) S' in
+        match i with
+        | increment la => largUpdate la (OP.opDenote _ plus
+                                                     (argDenote S la)
+                                                     (argDenote S (Ast.const (one _))))
                                      S
-      | clobber ra     => S
-      end.
+        | decrement la => largUpdate la (OP.opDenote _ minus
+                                                     (argDenote S la)
+                                                     (argDenote S (Ast.const (one _))))
+                                     S
+        | assign ass => match ass with
+                        | extassign4 op la1 la2 ra1 ra2 ra3 =>
+                          updatePair la1 la2 (OP.opDenote _ op
+                                                          (argDenote S ra1)
+                                                          (argDenote S ra2)
+                                                          (argDenote S ra3))
+                        | extassign3 op la1 la2 ra1 ra2     =>
+                          updatePair la1 la2 (OP.opDenote _ op
+                                                          (argDenote S ra1)
+                                                          (argDenote S ra2))
+                        | assign3 op la ra1 ra2 => largUpdate la (OP.opDenote _ op
+                                                                              (argDenote S ra1)
+                                                                              (argDenote S ra2))
+                                                              S
+                        | assign2 op la ra1     => largUpdate la (OP.opDenote _ op
+                                                                              (argDenote S ra1))
+                                                              S
+                        | update2 op la ra1     => largUpdate la (OP.opDenote _ op
+                                                                              (argDenote S la)
+                                                                              (argDenote S ra1))
+                                                              S
+                        | update1 op la         => largUpdate la (OP.opDenote _ op
+                                                                              (argDenote S la))
+                                                              S
+                        end
+        | moveTo x ix ra => largUpdate (index x ix) (@argDenote S _ _ rval (var ra))
+                                       S
+        | clobber ra     => S
+        end.
 
 
     End InstructionDenote.
@@ -147,62 +139,66 @@ Module Semantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS
 
                  (A1 /\ A2, (A1 -> C1) /\ (A1 /\ A2 -> C2))
 
-        with the annotations being instantiated with the context vs
+        with the annotations being instantiated with the stores
         at the corresponding program points.
        *)
 
       Definition spec := (Prop * Prop)%type.
 
-      Variable st : context v.
-      Variable ost : context v.
+      Variable st : store.
+      Variable ost : store.
 
       Definition annotationDenote (a : annotation v) (s : spec) : spec :=
         let (ass, cl) := s in
-        let ctxtP := (st, ost) in
+        let ctxtP := (eval st, eval ost) in
         (fun na => ((ass /\ na, cl /\ (ass -> na)))) (a ctxtP).
 
     End Annotate.
+
+    Section CodeDenote.
+
+      (* The Type capturing the program state *)
+      Definition state := (store * store * spec)%type.
+
+      Definition clDenote (cl : codeline v) (s : state) : state :=
+        let '(st, ost, sp) := s in
+        match cl with
+        | assert a => (fun na => (st, ost, na)) (annotationDenote st ost a sp)
+        | inst  i => (fun ni => (ni, ost, sp)) (instructionDenote i st)
+        end.
+
+      Definition codeDenote c s : state := List.fold_left
+                                             (fun s i => (clDenote i) s)
+                                             c
+                                             s.
+      (*
+    Definition unchanged b : Prop := forall st `(ty : type k) (x : v ty),
+        List.In (existT _ _ (existT _ _ x)) (changed b) \/
+        (fst (fst st)) _ _ x = (fst (fst (codeDenote b st))) _ _ x.
+       (* Try this with Ensembles *)
+       *)
+
+      Definition codeCheck c init : Prop :=
+        (fun x => snd (snd x)) (codeDenote c (init, init, (True, True))).
+
+      Definition SAT c    := forall init, codeCheck c init.
+
+    End CodeDenote.
 
   End Semantics.
 
 End Semantics.
 
-Module CodeSemantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS W).
+Module StandardSemantics := Semantics StandardWord StandardConsts StandardWordOps.
+
+Require Import Verse.Semantics.ScopeStore.
+
+Module SemanticTactics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS W).
 
   Module Export S := Semantics W CW O.
-  Import S.
-
-  Section CodeDenote.
-
-    Variable v : VariableT.
-    Variable v_eqb : forall {k} {ty : type k} (v1 v2 : v ty), bool.
-
-    (* The Type capturing the program state *)
-    Definition state := (context v * context v * spec)%type.
-
-    Definition clDenote (cl : codeline v) (s : state) : state :=
-      let '(st, ost, sp) := s in
-      match cl with
-      | assert a => (fun na => (st, ost, na)) (annotationDenote st ost a sp)
-      | inst  i => (fun ni => (ni, ost, sp)) (instructionDenote v_eqb i st)
-      end.
-
-    Definition codeDenote c s : state := List.fold_left
-                                           (fun s i => (clDenote i) s)
-                                           c
-                                           s.
-
-    Definition codeCheck c init : Prop :=
-      (fun x => snd (snd x)) (codeDenote c (init, init, (True, True))).
-
-    Definition SAT c    := forall init, codeCheck c init.
-
-  End CodeDenote.
 
   Definition genSAT `(vT : Vector.t (some type) n) c
-    := SAT (@scopeVar_eqb _ vT) (@fillDummy (@code _) _ vT c).
-
-  Arguments codeDenote [v] _ _.
+    := SAT (scopeStore vT) (@fillDummy (@code _) _ vT c).
 
   Let Fixpoint swapScope t (v : GenVariableT t)
       n (vT : Vector.t (some t) n) typ C {struct vT}
@@ -291,7 +287,7 @@ Module CodeSemantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMAN
     [shelve | rearrScope x | idtac];
     simpl in tmp; parametrize tmp.
 
-  (* Destruct the variable context v for easier access to valuations *)
+  (* Destruct the variable store for easier access to valuations *)
   Ltac breakStore :=
     repeat
       match goal with
@@ -304,15 +300,15 @@ Module CodeSemantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMAN
   Ltac simplify :=
     repeat
       (match goal with
-      | |- ?p              => unfold p
-      | a : _ * _ |- _     => simpl in a; destruct a
-      | |- forall _ : _, _ => intro
-      | |- exists _ : _, _ => eapply ex_intro
-      | |- _ /\ _          => constructor
-      | |- _ = _           => constructor
-      | _                  => simpl in *
-      end; repeat autounfold).
+       | |- ?p              => unfold p
+       | a : _ * _ |- _     => simpl in a; destruct a
+       | |- forall _ : _, _ => intro
+       | |- exists _ : _, _ => eapply ex_intro
+       | |- _ /\ _          => constructor
+       | |- _ = _           => constructor
+       | _                  => simpl in *
+       end; repeat autounfold).
 
-End CodeSemantics.
+End SemanticTactics.
 
-Module StandardSemantics := CodeSemantics StandardWord StandardConsts StandardWordOps.
+Module StandardTactics := SemanticTactics StandardWord StandardConsts StandardWordOps.
