cb6153a29e2710ba431ae6a927a80cadf85b5673
dangabhi <ahdang@iitk.ac.in>
2018-11-17 15:43:58 +0530

    portableAnnotated cleaned up a bit

--- src/Verse/CryptoLib/sha2/c/portableAnnotated.v
+++ src/Verse/CryptoLib/sha2/c/portableAnnotated.v
@@ -399,60 +399,43 @@ Module SHA2 (C : CONFIG).
       |}.
   End Program.
 
+  (* Write a scoped version of STEP *)
   Definition scSTEP v k t tp temp m a b c d e f g h
     := STEP v t tp temp {| A := a; B := b; C := c; D := d; E := e; F := f; G := g; H := h |} m k.
 
+  (* Extract the proof obligation from scSTEP *)
   Definition toProve : Prop.
     exParamProp scSTEP.
   Defined.
 
+  (* A lemma for correctness of the Sigma optimization *)
+  Lemma SigmaCorrect r0 r1 r2 (incr : r0 <= r1 <= r2) (w : typeDenote Word : Type)
+    : RotRW r0 (XorW (RotRW (r1 - r0) (XorW (RotRW (r2 - r1) w) w)) w) =
+      XorW (XorW (RotRW r2 w) (RotRW r1 w)) (RotRW r0 w).
+  Proof.
+    repeat rewrite rotRDistrXor;
+      repeat rewrite rotRCompose;
+      repeat rewrite Minus.le_plus_minus_r;
+      [ trivial | pose increasing_R's; easy ..].
+  Qed.
+
   Definition proof : toProve.
-    time (
-        unfold toProve;
-        unfold genSAT;
-        unfold SAT;
-        breakStore;
-        lazy -[wordOpDenote RotRW RotLW ShiftRW ShiftLW XorW AndW OrW NegW
-                fromNibbles
-                numBinOp numUnaryOp numBigargExop numOverflowBinop
-                Nat.add Nat.sub Nat.mul Nat.div Nat.pow
-                N.add N.sub N.mul N.div N.div_eucl N.modulo
-                Ox nth replace];
-    repeat
-      (match goal with
-       | |- _ /\ _          => constructor
-       | |- _ -> _          => intro
-       | H : _ /\ _ |- _    => destruct H
-       | H : True |- _      => clear H
-       | |- True            => constructor
-       | |- ?x = ?x         => trivial
-       end)).
+    simplify.
     (* Sigma *)
-    unfold wordOpDenote;
-      repeat rewrite rotRDistrXor;
-      repeat rewrite rotRCompose;
-      repeat rewrite Minus.le_plus_minus_r;
-      [ trivial | pose increasing_R's; easy ..].
+    apply SigmaCorrect. apply increasing_R's.
     (* Sigma *)
-    unfold wordOpDenote;
-      repeat rewrite rotRDistrXor;
-      repeat rewrite rotRCompose;
-      repeat rewrite Minus.le_plus_minus_r;
-      [ trivial | pose increasing_R's; easy ..].
+    apply SigmaCorrect. apply increasing_R's.
     (* Maj *)
-    unfold wordOpDenote.
     rewrite AndComm.
     now rewrite AndDistrOr.
     (* Ring *)
     rewrite H3.
-    unfold wordOpDenote.
     Add Ring Here : (mod_semi_ring (4 * (2 * (2 ^ WordSize)))).
     now ring_simplify.
 
     rewrite H3.
     rewrite H2.
     rewrite H1.
-    unfold wordOpDenote.
     now ring_simplify.
   Qed.
 
