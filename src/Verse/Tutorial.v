e969c455da8029360998a9940c2b5560d07602e1
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-01-22 14:51:59 +0530

    updates to the tutorial.

--- src/Verse/Tutorial.v
+++ src/Verse/Tutorial.v
@@ -1,3 +1,9 @@
+(** printing wi %w_i% #w<sub>i</sub>#  *)
+(** printing wi16 %w_{i+16}% #w<sub>i+16</sub>#  *)
+(** printing wi14 %w_{i+14}% #w<sub>i+14</sub>#  *)
+(** printing wi9 %w_{i+9}% #w<sub>i+9</sub>#  *)
+(** printing wi1 %w_{i+1}% #w<sub>i+1</sub>#  *)
+
 (** * The Verse EDSL.
 
 Implementing cryptographic primitives in a High level language is
@@ -10,67 +16,104 @@ problematic for two reasons.
 
 Due to this unfortunate situation, cryptographic libraries, even for a
 high level language like Haskell, has in it a lots of hand written C
-and assembly code fragment. This is not a nice situation to be in.
+and assembly code fragment. This is not a nice situation to be in.  As
+a solution to this problem, we propose Verse, an embedded domain
+specific language designed for writing cryptographic primitives. This
+module serves as a short tutorial for Verse.
 
-As a solution to this problem, we propose Verse, an embedded domain
-specific language specifically designed for writing cryptographic
-primitives. This module serves as a short tutorial for Verse.
 
-*)
+ *)
 
 (** ** Getting started.
 
-Our aim is to make Verse's surface syntax clean enough so that
-
-- Cryptographic implementations written in Verse should be readable
-  even to a user who does not know Coq. Cryptographic libraries
-  require a great deal of expert review. It should be possible for a
-  cryptographic experts who might not know much of Coq to follow the
-  intricacies of the code. To this end, we have made the surface
-  syntax of Verse mimic the syntax of a typical imperative language
-  with symbolic variables, assignment statements and array indexing.
-
-- Cryptographic implementations should be writable by a programmer
-  with a working knowledge of Coq, without worrying about the
-  internals of Coq. The verse language is designed to be correct by
-  construction which throws up a lot of proof obligations. We have
-  automatic tactics to dispose these of.
-
-
-Therefore, we believe that you can get started with verse with almost
-no knowledge of coq or verse. If you want to hack on verse code, you
-will need coq (currently tested with 8.6 and 8.7) and the coq library
-CoLoR.  The suggested means of developing verse code is using
-opam. However, the CoLoR library has issues with the opam-2. So we
-recommend using opam-1.2.2. We also recommend the use of emacs and the
-proof general mode for editing.
+We believe that one can get started with verse with minimal knowledge
+of coq or verse. If you want to hack on verse code, you will need coq
+(currently tested with 8.6 and 8.7) and the coq library CoLoR.  The
+suggested means of developing verse code is using opam. However, the
+CoLoR library has issues with the opam-2. So we recommend using
+opam-1.2.2. We also recommend the use of emacs and the proof general
+mode for editing.
 
 *)
 
 (** ** A quick tour.
 
+
+Cryptographic primitives come in two variety: bulk primitives and
+fixed input ones. Primitives like ciphers, cryptographic hash
+functions etc takes input as a stream of fixed sized chunks (called
+blocks) and processes it and are examples for the former.  On the
+other hand, the operations needed for asymmetric key primitives like
+point multiplication in an elliptic curve are examples for the
+latter. Being designed specifically for crypto-primitives, verse only
+supports two kinds of functions.
+
+- An iterator which can be thought of as a particular transformation,
+  applied individually on each block of data in a stream of blocks, or
+
+- Straight line programs which are a sequence of assembly language
+  statements.
+
+In particular, there are no facilities for performing any kind of
+control flow except the implicit ones when the iterator loops over the
+blocks. Not only are these two forms general enough to write
+cryptographic primitives, restricting to such functions means that we
+would not inadvertently use a secret information to execute a branch
+thereby leaking information about secrets through the branch
+predication.
+
+
+
+ *)
+
+(** *** Straight line functions
+
+Let us give an example of a message scheduling for a sha2 like hash,
+deliberately simplified to illustrate some of the constructs of the
+language. The message is an array of 16x64-bit unsigned integers that
+is expanded using the rule [wi16 = wi + wi14 + wi9 + wi1].
+
+
+*)
+
+(*
+
 Verse is really a low level language with an instruction set that can
-be directly translated (almost 1:1) to the instruction set of a
-typical general purpose machine architecture like x86. So one can
-really program pretty close to the machine including controlling
-aspects like register allocation. This is very important because we
-would like to exactly predict the emitted code so that we can control
-side channel leakages. However, verse exploits the fact that it is
-embedded in Coq to give the programmer a markedly high level
-experience.
+be directly translated almost 1:1 to the instruction set of a typical
+general purpose machine architecture like x86. This enables one to
+write programs that are pretty close to the machine including
+controlling aspects like register allocation. Such a low level
+interface is a necessary so that we can exactly predict the emitted
+code and control side channel leakages. Yet verse is a typed language
+with a type system rich enough to even prevent errors in array
+indexing and endian conversion. One should therefore think of verse as
+a typed assembly language like LLVM (but with types that are in some
+aspects more powerful).
 
-- Verse is equipped with a type system that is rich enough to even
-  prevent errors in array indexing and endian conversion.
+Despite the above restrictions, we would like the programming
+experience to be high level. We will exploit the embedded nature of
+verse to our advantage.
 
-- One can generate Verse code using functions written in Gallina which
-  behave like macros for code generation
+- Verse programs are represented as inductive types in Gallina hand
+  hence we can write code generations and manipulation functions in
+  Gallina which can act like macros. The Verse system comes with some
+  standard such macros that implement things like loops etc.
 
-- Many of the type safety property in verse is achieved by making the
-  verse language correct by construction. However, this does not
-  become proof burden to the user thanks to Ltac.
+- The type safety property in verse is achieved by making the verse
+  language correct by construction. However, this does not become
+  proof burden to the programmer due to tatics that we have exported
+  from Verse.
+
+- Using the "Notation" feature of coq, the surface syntax of Verse
+  mimic the syntax of a typical imperative language with symbolic
+  variables, assignment statements and array indexing. This enhances
+  the readability and writeability of verse programs.
 
 - Finally, the interactive mode like proof general, gives an IDE like
   experience when building complicated programs.
+
+
+
 
  *)
 
