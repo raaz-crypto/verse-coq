cc7ed48e30a5c2a2132fc1f7509e9f16a3a3e06f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-03-03 11:25:08 +0530

    simplified operators to have just one arity.

--- src/Verse/Language/Operators.v
+++ src/Verse/Language/Operators.v
@@ -15,30 +15,28 @@ either [unary] or [binary].
 
 *)
 
-Inductive arity  := binary | unary
+Inductive arity  := binary | unary.
 
-Inductive op    : arity -> arity -> Type :=
-| plus    : op unary binary
-| minus   : op unary binary
-| mul     : op unary binary
-| quot    : op unary binary
-| rem     : op unary binary
-| bitOr   : op unary binary
-| bitAnd  : op unary binary
-| bitXor  : op unary binary
-| bitComp : op unary unary
-| rotL    : nat -> op unary unary
-| rotR    : nat -> op unary unary
-| shiftL  : nat -> op unary unary
-| shiftR  : nat -> op unary unary
-| nop     : op unary unary
+Inductive op : arity -> Type :=
+| plus    : op binary
+| minus   : op binary
+| mul     : op binary
+| quot    : op binary
+| rem     : op binary
+| bitOr   : op binary
+| bitAnd  : op binary
+| bitXor  : op binary
+| bitComp : op unary
+| rotL    : nat -> op unary
+| rotR    : nat -> op unary
+| shiftL  : nat -> op unary
+| shiftR  : nat -> op unary
+| nop     : op unary
 .
 
-Notation simop := (op unary).
-Notation exop  := (op binary).
-Notation binop := (simop binary).
-Notation uniop := (simop unary).
-Notation triop := (simop ternary).
+Definition binop := op binary.
+Definition uniop := op unary.
+
 
 (** *** Semantics of operators.
 
@@ -49,19 +47,10 @@ from its meaning on word types. We start with the meaning of [arity].
 
 *)
 
-Local Definition lArityDenote a t : Type  :=
+Definition arityDenote a (t : Type) :=
   match a with
-  | unary    => t
-  | binary   => t * t
-  | ternary  => t * t * t
-  end.
-
-Definition arityDenote la ra (t : Type) :=
-  let dest := lArityDenote la t in
-  match ra with
-  | unary   => t -> dest
-  | binary  => t -> t -> dest
-  | ternary => t -> t -> t -> dest
+  | unary   => t -> t
+  | binary  => t -> t -> t
   end.
 
 
@@ -76,7 +65,7 @@ words to all types.
 (* Require Import Verse.Types.*)
 
 Module Type OP_SEMANTICS (W : WORD_SEMANTICS).
-  Parameter wordOpDenote    : forall la ra n, op la ra -> arityDenote la ra (W.wordDenote n).
+  Parameter wordOpDenote    : forall a n, op a -> arityDenote a (W.wordDenote n).
 End OP_SEMANTICS.
 
 Module OpDenote (W : WORD_SEMANTICS)(O : OP_SEMANTICS W).
@@ -103,26 +92,19 @@ Module OpDenote (W : WORD_SEMANTICS)(O : OP_SEMANTICS W).
 
   End VectorAux.
 
-  Local Definition liftOpDenote m typ la ra : (op la ra -> arityDenote la ra typ) ->
-                                              op la ra -> arityDenote la ra (Vector.t typ m) :=
-    match la, ra return (op la ra -> arityDenote la ra typ) ->
-                        op la ra ->
-                        arityDenote la ra (Vector.t typ m)
+  Local Definition liftOpDenote m typ ar : (op ar -> arityDenote ar typ) ->
+                                           op ar -> arityDenote ar (Vector.t typ m) :=
+    match ar return (op ar -> arityDenote ar typ) ->
+                        op ar ->
+                        arityDenote ar (Vector.t typ m)
     with
-    | unary, unary       => fun opD op v => Vector.map (opD op) v
-    | binary, unary      => fun opD op v => opVecSplit (Vector.map (opD op) v)
-    | ternary, unary     => fun opD op v => opVecSplit2 (Vector.map (opD op) v)
-    | unary, binary      => fun opD op v1 v2 => Vector.map2 (opD op) v1 v2
-    | binary, binary     => fun opD op v1 v2 => opVecSplit (Vector.map2 (opD op) v1 v2)
-    | ternary, binary    => fun opD op v1 v2 => opVecSplit2 (Vector.map2 (opD op) v1 v2)
-    | unary, ternary     => fun opD op v1 v2 v3 => Vector.map2 apply (Vector.map2 (opD op) v1 v2) v3
-    | binary, ternary    => fun opD op v1 v2 v3 => opVecSplit (Vector.map2 apply (Vector.map2 (opD op) v1 v2) v3)
-    | ternary, ternary   => fun opD op v1 v2 v3 => opVecSplit2 (Vector.map2 apply (Vector.map2 (opD op) v1 v2) v3)
+    | unary       => fun opD op v => Vector.map (opD op) v
+    | binary      => fun opD op v1 v2 => Vector.map2 (opD op) v1 v2
     end.
 
-  Fixpoint opDenote {la ra : arity}{k : kind}(t : type k)
-    : op la ra -> arityDenote la ra (typeDenote t) :=
-    match t as t0 return op la ra -> arityDenote la ra (@typeDenote _ tyDenote _ t0) with
+  Fixpoint opDenote {ar : arity}{k : kind}(t : type k)
+    : op ar -> arityDenote ar (typeDenote t) :=
+    match t as t0 return op ar -> arityDenote ar (@typeDenote _ tyDenote _ t0) with
     | word n            => wordOpDenote n
     | multiword m n     => liftOpDenote (2 ^ m) (@typeDenote _ tyDenote _ (word n)) (O.wordOpDenote n)
     | array n _ tw      => liftOpDenote n (typeDenote tw) (opDenote tw)
@@ -136,14 +118,12 @@ Module StandardWordOps <: OP_SEMANTICS (StandardWord).
 
   Definition OpError := False.
 
-  Definition wordOpDenote la ra n (o : op la ra) : arityDenote la ra (StandardWord.wordDenote n) :=
-    match o in op la0 ra0 return arityDenote la0 ra0 (StandardWord.wordDenote n) with
+  Definition wordOpDenote ar n (o : op ar) : arityDenote  ar (StandardWord.wordDenote n) :=
+    match o in op ra0 return arityDenote ra0 (StandardWord.wordDenote n) with
     | plus         => fun a b => Word.numBinOp N.add a b
     | minus        => fun a b => Word.numBinOp N.sub a b
     | mul          => fun a b => Word.numBinOp N.mul a b
-    | exmul        => fun a b => Word.numOverflowBinop N.mul a b
     | quot         => fun a b => Word.numBinOp N.div a b
-    | eucl         => fun a b c => Word.numBigargExop N.div_eucl a b c
     | rem          => fun a b => Word.numBinOp N.modulo a b
     | bitOr        => fun a b => OrW a b
     | bitAnd       => fun a b => AndW a b
