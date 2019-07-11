924c137292bbb5f8443afe003e87e77b351ba263
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-07-11 15:24:19 +0530

    move the verse types to the AST module.

--- src/Verse/Language/Ast.v
+++ src/Verse/Language/Ast.v
@@ -1,4 +1,91 @@
+
+(** printing power2m   $ 2^m     $ # 2<sup> m   </sup> # *)
+(** printing power2n   $ 2^n     $ # 2<sup> n   </sup> # *)
+(** printing power2p3  $ 2^3     $ # 2<sup> 3   </sup> # *)
+(** printing power2np3 $ 2^{n+3} $ # 2<sup> n+3 </sup> # *)
+
+
+(** * The abstract syntax tree of Verse.
+
+This module exposes the abstract syntax of the verse programming
+language. The design takes the following points into consideration.
+
+- A large number of instructions are shared across architectures. This
+  include instructions that perform various arithmetic operations,
+  bitwise operations etc.
+
+- Certain architecture support various special registers like xmm
+  registers, special instructions like native AES operations.
+
+
+The design gives a portable way of expressing the former and
+parameterise over the latter. Let us include arithmetic operators
+first.
+
+Some correctness and safety properties for features like array
+indexing has been built into the ast and thus gives a correct by
+construction style of AST.
+
+
+We begin by defining the types for the language.
+
+ *)
+
+
 (* begin hide *)
+
+Require Verse.TypeSystem.
+Require Verse.Nibble.
+Require Import Arith.
+Import Nat.
+Set Implicit Arguments.
+
+(* end hide *)
+
+(** ** The type system for Verse.
+
+The Verse EDSL is a typed machine language consisting of [word]s,
+[multiword]s, and [array]s of which the first two, i.e. [word]s and
+[multiword]s, can reside in the registers of the machine where as
+[array]s are necessarily stored in the machine memory. The kind system
+indicates this distinction in type.
+
+*)
+Inductive endian : Type := bigE | littleE | hostE.
+
+
+Inductive type       : TypeSystem.kind -> Type :=
+| word               : nat -> type TypeSystem.direct
+| multiword          : nat -> nat    -> type TypeSystem.direct
+| array              : nat -> endian -> type TypeSystem.direct -> type TypeSystem.memory
+.
+
+Definition const (t : type TypeSystem.direct) :=
+  match t with
+  | word n => Nibble.bytes (2^n)
+  | multiword m n => Vector.t (Nibble.bytes (2^n))  (2 ^ m)
+  end.
+
+Canonical Structure verse_type_system : TypeSystem.typeSystem := TypeSystem.TypeSystem type const.
+
+
+(** Compute the size of a type in bytes. *)
+Fixpoint sizeOf {k} (ty : type k) :=
+  match ty with
+  | word n         => 2 ^ n
+  | multiword m n  => 2 ^ m * 2 ^ n
+  | array n _ tw => n * sizeOf tw
+  end.
+
+
+(** Often we need to existentially quantify over types and other
+    objects. This definition is just for better readability.
+ *)
+
+Definition some {P: Type} (A : P -> Type) := sigT A.
+
+
+(*
 Require Import Verse.Types.
 Require Import Verse.Types.Internal.
 Require Import Verse.Syntax.
@@ -15,20 +102,6 @@ Set Implicit Arguments.
 (* end hide *)
 
 (** * The Verse language as an inductive data type.
-
-This module exposes the abstract syntax of the verse programming
-language. The design takes the following points into consideration.
-
-- A large number of instructions are shared across architectures. This
-  include instructions that perform various arithmetic operations,
-  bitwise operations etc.
-
-- Certain architecture support various special registers like xmm
-  registers, special instructions like native AES operations.
-
-The design gives a portable way of expressing the former and
-parameterise over the latter. Let us include arithmetic operators
-first.
 
 *)
 
@@ -286,3 +359,4 @@ Arguments decrement [v ty] _.
 Arguments moveTo [v  b e ty] _ _ _.
 Arguments clobber [v k ty ] _.
 (* end hide *)
+*)
