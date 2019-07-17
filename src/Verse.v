f517106e23fe3c713a24d1edf7ab8d221b7a5bc1
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-07-17 12:04:38 +0530

    added top level modules.

--- src/Verse.v
+++ src/Verse.v
@@ -1,15 +1,15 @@
 Require Export Verse.Language.
-Require Export Verse.Types.
-Require Export Verse.Types.Internal.
-Require Export Verse.Word.
-Require Export Verse.Error.
-Require Export Verse.Syntax.
-Require Export Verse.PrettyPrint.
-Require Export Verse.Nibble.
-Require Export String.
-Require Export Nat.
-Require Vector.
-Export Vector.VectorNotations.
-Delimit Scope vector_scope with vector.
-Require Export List.
-Coercion Vector.to_list : Vector.t >-> list.
+(* Require Export Verse.Types. *)
+(* Require Export Verse.Types.Internal. *)
+(* Require Export Verse.Word. *)
+(* Require Export Verse.Error. *)
+(* Require Export Verse.Syntax. *)
+(* Require Export Verse.PrettyPrint. *)
+(* Require Export Verse.Nibble. *)
+(* Require Export String. *)
+(* Require Export Nat. *)
+(* Require Vector. *)
+(* Export Vector.VectorNotations. *)
+(* Delimit Scope vector_scope with vector. *)
+(* Require Export List. *)
+(* Coercion Vector.to_list : Vector.t >-> list. *)
--- src/Verse/Language.v
+++ src/Verse/Language.v
@@ -1,3 +1,2 @@
 Require Export Verse.Language.Ast.
-Require Export Verse.Language.Utils.
 Require Export Verse.Language.Pretty.
