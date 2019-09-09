894e331883e478f7c9c94bdeef0e91deb84fdb87
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-09-09 22:31:05 +0530

    modified C test pretty printing code.

--- src/Verse/Target/C/Test.v
+++ src/Verse/Target/C/Test.v
@@ -3,9 +3,11 @@ Require Import Verse.Language.Types.
 Require Import Verse.Target.C.Ast.
 Require Import Verse.Target.C.Pretty.
 Require Verse.Language.Ast.
-Import Ast.Internal.
+Import Ast.Expr.
+Import Nibble.
 
 Open Scope c_scope.
-Axiom x : Internal.expr.
+Axiom x : Expr.expr.
 
 Compute (convert_to bigE uint32_t (rotateL uint32_t (x, 2))).
+Compute (verse_u8 (Ox1,Ox2)).
