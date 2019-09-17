2a1f95741857d2de17e4c3a4aec5da2736949638
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-09-18 01:16:29 +0530

    compilation of type translated verse code to C.

--- src/Verse/Target/C/Translate.v
+++ src/Verse/Target/C/Translate.v
@@ -35,3 +35,84 @@ End Internal.
 
 Canonical Structure c_type_compile : typeCompile verse_type_system c_type_system
   := verseTranslation Internal.trType Internal.trConst.
+
+
+
+Require Verse.Language.Ast.
+Import Verse.Target.C.Ast.Expr.
+Require Vector.
+Import Vector.VectorNotations.
+
+Module CompilerInternals.
+
+  Fixpoint trExpr {ty} (e : Ast.expr cvar ty) : expr :=
+    match e with
+    | Ast.cval   c     => verse_const _ c
+    | Ast.valueOf le   => match le with
+                         | Ast.var v => v
+                         | @Ast.deref _ _ _ _ endn arr (exist _ i _) =>
+                           let arrI := index arr i in
+                           match endn with
+                           | hostE => arrI
+                           | _     => convert_from endn ty arrI
+                           end
+                         end
+    | @Ast.app _ _ _  _ o v
+      => let args := Vector.map trExpr v in
+        match o with
+        | Ast.cop co => app co
+        | Ast.rotL n => fun cve => rotateL ty (Vector.hd cve, n)
+        | Ast.rotR n => fun cve => rotateR ty (Vector.hd cve, n)
+        end args
+    end.
+
+  Definition trAssign {ty} (le : Ast.lexpr cvar ty) (ex : Ast.expr cvar ty) :=
+    let endianConversion endn cex :=
+        match endn, cex with
+        | hostE, _                            => cex
+        | littleE, convert_from littleE _ cep => cep
+        | bigE, convert_from bigE _ cep       => cep
+        | _, _                                => convert_to endn ty cex
+        end in
+
+    match le with
+    | Ast.var v => assign v (trExpr ex)
+    | @Ast.deref _ _ _ _ endn arr (exist _ i _)
+      => assign (index arr i) (endianConversion endn (trExpr ex))
+    end.
+
+  (** This definition is giving some warning in terms of implicits please check *)
+  Definition trInst ty (inst : Ast.instruction cvar ty) : statement + {TranslationError}
+    := let getCOP {n}(o : Ast.op n)
+           := match o with
+              | Ast.cop co => {- co -}
+              | _          => error (CouldNotTranslateBecause inst UpdatesNotForRotatesInC)
+              end in
+       let handleUpdate le (func : expr -> statement) :=
+           match le with
+           | Ast.var v => {- func v -}
+           | @Ast.deref _ _ _ _ endn arr (exist _ i _)
+             => match endn with
+               | hostE => {- func (index arr i) -}
+               | _     => error (CouldNotTranslateBecause inst UpdatesNeedHostEndian)
+               end
+           end
+       in
+       match inst with
+       | Ast.assign le ex     => {- trAssign le ex -}
+       | Ast.update o le vex  => co <- getCOP o;
+                                  handleUpdate le (fun lhs => update co lhs (Vector.map trExpr vex))
+       | Ast.increment le     => handleUpdate le increment
+       | Ast.decrement le     => handleUpdate le decrement
+       | Ast.moveTo le v      => {- trAssign le (Ast.valueOf (Ast.var v)) -}
+       | _                    => error (CouldNotTranslateBecause inst ExplicitClobberNotInC)
+      end.
+
+  Definition trStatement (s : Ast.statement cvar) :=
+    match s with
+    | existT _ ty inst => trInst ty inst
+    end.
+
+  Definition trCode cs := merge _ _ (List.map trStatement cs).
+
+End CompilerInternals.
