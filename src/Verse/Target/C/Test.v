167ba689ed5d7b6ebe06a237a2304a5416c407b5
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-10-10 11:50:09 +0530

    added notation tests for declarations.

--- src/Verse/Target/C/Test.v
+++ src/Verse/Target/C/Test.v
@@ -7,7 +7,13 @@ Import Ast.Expr.
 Import Nibble.
 
 Open Scope c_scope.
-Axiom x : Expr.expr.
+
+Axiom x : cvar uint8_t.
+Axiom arr : cvar (array 42 uint16_t).
+Axiom ptr : cvar (ptrToArray 30 uint64_t).
+Compute (declare x).
+Compute (declare arr).
+Compute (declare ptr).
 
 Compute (convert_to bigE uint32_t (rotateL uint32_t (x, 2))).
 Compute (verse_u8 (Ox1,Ox2)).
