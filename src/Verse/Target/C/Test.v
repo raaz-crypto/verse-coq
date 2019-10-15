bc04474b5d6506fca8954e9b5338528399477280
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-10-15 14:46:57 +0530

    now runs the C pretty printing test.

--- src/Verse/Target/C/Test.v
+++ src/Verse/Target/C/Test.v
@@ -3,9 +3,12 @@ Require Import Verse.Language.Types.
 Require Import Verse.Target.C.Ast.
 Require Import Verse.Target.C.Pretty.
 Require Verse.Language.Ast.
+Require List.
+Import List.ListNotations.
 Import Ast.Expr.
 Import Nibble.
-
+Require Vector.
+Import Vector.VectorNotations.
 Open Scope c_scope.
 
 Axiom x : cvar uint8_t.
@@ -17,3 +20,19 @@ Compute (declare ptr).
 
 Compute (convert_to bigE uint32_t (rotateL uint32_t (x, 2))).
 Compute (verse_u8 (Ox1,Ox2)).
+Axiom myfunc : name.
+Axiom a : cvar uint8_t.
+Axiom b : cvar uint64_t.
+Axiom temp : cvar uint8_t.
+Axiom vardec : annotation.
+Axiom empty  : annotation.
+Notation "'variable' 'declarations'" := vardec (only printing, format " 'variable'  'declarations'  ").
+
+Definition e : Expr.expr := app mul [a ; app plus [a ; b]].
+Definition stmts :=
+  mkBlock vardec [ declareStmt (declare temp); declareStmt (declare a); update bitXor a [e]].
+Definition foo : function :=
+  void_function myfunc (declare a, declare b)
+                 stmts (Some (while b stmts))  (mkBlock empty []).
+
+Compute foo.
