f71b08fee9cb2b7a36aee2156ac04dc4605a3d1c
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-11-10 22:37:06 +0530

    compilation and translation for variable allocation.

--- src/Verse/Scope.v
+++ src/Verse/Scope.v
@@ -1,6 +1,5 @@
 Require Import Verse.Language.Types.
 Require Import Verse.TypeSystem.
-Require Import Verse.Variables.
 Require Vector.
 Import Vector.VectorNotations.
 
@@ -22,35 +21,39 @@ Section Scoped.
 
   Variable ts : typeSystem.
   Variable v  : Variables.U ts.
-  Arguments v [k].
 
-  (** Since variables themselves have types, a scoped code of [n]
-      variables themselves are parameterised by a vector of [n] types.
-      We call such vector of types as scopeType.
+  (** A scoped code of [n] variables, which themselves have [some]
+      type, are parameterised by a vector of [n] types (existentially
+      quantified over their kinds). We abuse the terminology type and
+      call such a vector of (existentially quantified) types as types
+      (of scopes).
 
    *)
+  Definition type n :=  Vector.t (some (typeOf ts)) n.
 
-  Definition scopeType n :=  Vector.t (some (typeOf ts)) n.
+  (*
+  Definition someVar s := qualified v s.
+   *)
 
-  Fixpoint scoped {n} (st : scopeType n)(CODE : Type) : Type :=
+  Fixpoint scoped {n} (st : type n)(CODE : Type) : Type :=
     match st with
-    | []                  => CODE
-    | existT _ _ ty :: lt => v ty -> scoped lt CODE
+    | []      => CODE
+    | s :: lt => qualified v s -> scoped lt CODE
     end.
 
   (** An allocation that can be used to satisfy a scoped object of
       [scopeType], [st].
    *)
 
-  Fixpoint allocation {n} (st : scopeType n) : Type :=
-    match n as n0 return scopeType n0 -> Type with
-    | 0   => fun _  => unit
-    | S m => fun v0 => (v (projT2 (Vector.hd v0)) * allocation (Vector.tl v0))%type
-    end st.
+  Fixpoint allocation {n} (st : type n) : Type :=
+    match st with
+    | [] => unit
+    | (x :: xs) => qualified v x * allocation xs
+    end.
 
   (** And such an allocation can be used to "fill" up the variables *)
 
-  Fixpoint fill {CODE} {n} {st : scopeType n}
+  Fixpoint fill {CODE} {n} {st : type n}
   : allocation st -> scoped st CODE -> CODE :=
     match st with
     | []             => fun a x => x
@@ -59,95 +62,73 @@ Section Scoped.
 
   Definition emptyAllocation : allocation [] := tt.
 
-  Definition uncurryScope {CODE} {n} {st : scopeType n}
+  Definition uncurryScope {CODE} {n} {st : type n}
     : scoped st CODE -> allocation st -> CODE
     := fun sc al =>  fill al sc.
 
 End Scoped.
 
-Arguments scoped [ts] _ [n].
-(*
+Arguments allocation [ts] v [n].
+Arguments scoped [ts] v [n].
 
-(** A way to apply functions inside a scope *)
-Fixpoint appScoped n (l : Vector.t (some t) n) T1 T2 (f : T1 -> T2)
-  : scoped l T1 -> scoped l T2 :=
-  match l with
-  | []                  => fun s1 => f s1
-  | existT ty :: lt => fun s1 => fun x : v ty => appScoped lt f (s1 x)
-  end.
+Require Import Verse.Error.
 
-(** A dummy VariableT that can help instantiate scoped code *)
-Inductive scopeVar : forall {n} (l : Vector.t (some type) n), VariableT :=
-| headVar m (v : Vector.t (some type) (S m)) : scopeVar v (projT2 (hd v))
-| restVar m (v : Vector.t (some type) (S m)) k (ty : type k) : scopeVar (tl v) ty
-                                                               -> scopeVar v ty.
+(** ** Translation/compilation for type of scopes *)
 
-Fixpoint mergeAllocation n1 n2 (l1 : Vector.t (some t) n1) (l2 : Vector.t (some t) n2)
-  : allocation l1 -> allocation l2 -> allocation (append l1 l2) :=
-  match l1 with
-  | []                  => fun _ a   => a
-  | existT ty :: lt => fun a1 a2 => (fst a1, mergeAllocation _ _ (snd a1) a2)
-  end.
+Module Types.
+
+  Definition translate src tgt
+             (tr : translator src tgt)
+             (n : nat)
+             (st : type src n)
+  : type tgt n := Vector.map (Types.Some.translate tr) st.
+  Arguments translate [src tgt] tr [n].
+
+  Definition result tgt n
+    := type (TypeSystem.result tgt) n.
 
 
-Fixpoint mergeScope T n1 n2 (l1 : Vector.t (some t) n1) (l2 : Vector.t (some t) n2)
-         {struct l1} : scoped l1 (scoped l2 T) -> scoped (append l1 l2) T :=
-  match l1 as l1' return scoped l1' (scoped l2 T)
-                         -> scoped (append l1' l2) T with
-  | []        => id
-  | existT ty :: lt  => fun x vt => mergeScope _ _ _ (x vt)
-  end.
+  Definition compile src tgt
+             (cr : compiler src tgt)
+             (n : nat)
+             (st : type src n)
+             : result tgt n
+    := translate cr st .
 
-End Scoped.
+  Arguments compile [src tgt] cr [n].
+End Types.
 
-Arguments headVar [m v].
-Arguments restVar [m v k ty] _.
-Arguments appScoped [t0 v n l T1 T2] _ _.
-Arguments mergeAllocation [t0 v n1 n2 l1 l2] _ _.
-Arguments mergeScope [t0 v T n1 n2 l1 l2] _.
+(** ** Translation/compilation for allocations *)
+Module Allocation.
 
+  Fixpoint translate src tgt
+             (tr : translator src tgt)
+             (v  : Variables.U tgt)
+             (n : nat)
+             (st : type src n)
+  :  allocation v (Types.translate tr st) ->
+     allocation (Variables.translate tr v) st
+    := match st with
+       | [] => fun H : unit => H
+       | Vector.cons _ x n0 xs
+         => fun a =>
+              let (f, r) := a in (Qualified.translate tr f, translate src tgt tr v n0 xs r)
+        end.
 
-Fixpoint mapAlloc v1 v2 (f : forall k (ty : type k), v1 _ ty -> v2 _ ty)
-         n (l : Vector.t (some type) n) : allocation v1 l -> allocation v2 l :=
-  match n return forall l : Vector.t (some type) n, allocation v1 l -> allocation v2 l with
-  | S n => fun l a1 => (f _ _ (fst a1), mapAlloc v1 v2 f (tl l) (snd a1))
-  | 0   => fun l _  => tt
-  end l.
+  Arguments translate [src tgt] tr [v n st].
+  Definition result tgt (v  : Variables.U tgt)
+             (n : nat)
+             (st : type (result tgt) n)
+             := allocation (Variables.result v) st.
 
-(* The dummy allocation for scoped code *)
-Fixpoint dummyAlloc {n} (l : Vector.t (some type) n) : allocation (scopeVar l) l :=
-  match n return forall l0 : Vector.t (some type) n, @allocation _ _ n l0 with
-   | S m => fun l0 => (@headVar _ l0, mapAlloc _ _ (@restVar _ l0) _ (dummyAlloc (tl l0)))
-   | 0   => fun _  => tt
-  end l.
+  Arguments result [tgt] v [n] st.
 
-(* Generic scoped code filled out with the dummy VariableT *)
-Definition fillDummy (CODE : VariableT -> Type) n (l : Vector.t (some type) n)
-                     (genC : forall v, scoped v l (CODE v)) :=
-  fill (dummyAlloc l) (genC (scopeVar l)).
+  Definition compile src tgt n
+             (cr : compiler src tgt)
+             (v : Variables.U tgt)
+             (st : type src n)
+             (res : result v (Types.compile cr st))
+    : allocation (Variables.compile cr v) st
+    := translate cr res.
 
-Arguments fillDummy [CODE n l] _.
-
-
-(* A variable type that us used as a proxy. This variable is *)
-
-Inductive ProxyVar : VariableT :=
-| ProxyV : forall k (ty : type k), ProxyVar ty.
-
-Arguments ProxyV [k] _.
-
-Ltac declare ps  := match (type of ps) with
-                    | VariableT -> _ => apply (ps ProxyVar);
-                                repeat match goal with
-                                       | [ |- @ProxyVar ?K ?T] => exact (@ProxyV K T)
-                                       end
-                    | _ => exact []
-                    end.
-
-Notation "(--)"             := (tt).
-Notation "(- x , .. , z -)" := (pair x .. (pair z tt) ..).
-
-Ltac assignRegisters regs  := exact (fun _ => regs).
-Ltac statements       s     := exact s.
-
-*)
+End Allocation.
