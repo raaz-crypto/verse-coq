17550c8a2d05e64058e82a442be0767036d70b06
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-11-23 00:58:42 +0530

    use section + Variables instead of Axiom
    Axioms should make people jittery. However, Variables are cool ;-)

--- src/Verse/Target/C/Test.v
+++ src/Verse/Target/C/Test.v
@@ -9,35 +9,39 @@ Import Nibble.
 Require Vector.
 Import Vector.VectorNotations.
 
-Axiom x : cvar uint8_t.
-Axiom arr : cvar (array 42 uint16_t).
-Axiom ptr : cvar (ptrToArray 30 uint64_t).
+Section Variables.
+  Variable x : cvar uint8_t.
+  Variable arr : cvar (array 42 uint16_t).
+  Variable ptr : cvar (ptrToArray 30 uint64_t).
 
-Definition index_ptr := Expr.ptrDeref ptr.
-Axiom myfunc : name.
-Axiom a : cvar uint8_t.
-Axiom b : cvar uint64_t.
-Axiom temp : cvar uint8_t.
 
-Definition e : Expr.expr := Expr.app mul [a ; Expr.app plus [a ; b]].
-Definition lDec := [ declare temp; declare a; declare ptr ]%list.
+  Variable myfunc : name.
+  Variable a : cvar uint8_t.
+  Variable b : cvar uint64_t.
+  Variable temp : cvar uint8_t.
 
-Definition stmts := [ assign (Expr.index (Expr.ptrDeref(ptr)) 2) e;
-                      update a bitAnd [e]%vector
-                    ]%list.
+  Definition index_ptr := Expr.ptrDeref ptr.
 
-(*
-Compute (declare x).
-Compute (declare arr).
-Compute (declare ptr).
-Compute (index (index_ptr) 10).
-Compute (convert_to bigE uint32_t (rotateL uint32_t (x, 2))).
-Compute (verse_const uint8_t (Ox1,Ox2)).
- *)
+  Definition e : Expr.expr := Expr.app mul [a ; Expr.app plus [a ; b]].
+  Definition lDec := [ declare temp; declare a; declare ptr ]%list.
 
-Require Import Verse.Print.
-Require Import Verse.Target.C.Pretty.
+  Definition stmts := [ assign (Expr.index (Expr.ptrDeref(ptr)) 2) e;
+                          update a bitAnd [e]%vector
+                      ]%list.
 
-Goal to_print (function myfunc (params [declare temp; declare temp]) (Braces stmts)).
-  print.
-Abort.
+  (*
+    Compute (declare x).
+    Compute (declare arr).
+    Compute (declare ptr).
+    Compute (index (index_ptr) 10).
+    Compute (convert_to bigE uint32_t (rotateL uint32_t (x, 2))).
+    Compute (verse_const uint8_t (Ox1,Ox2)).
+   *)
+
+  Require Import Verse.Print.
+  Require Import Verse.Target.C.Pretty.
+
+  Goal to_print (function myfunc (params [declare temp; declare temp]) (Braces stmts)).
+    print.
+  Abort.
+End Variables.
