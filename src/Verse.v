b2f0efbd5214cb52a62b499a6a266a3d2d607888
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-01-27 01:42:23 +0530

    top level verse definitions added.

--- src/Verse.v
+++ src/Verse.v
@@ -1,17 +1,29 @@
+Require Export String.
+Require Export Verse.Nibble.
 Require Export Verse.Language.
 Require Export Verse.TypeSystem.
+Require Vector.
+Export Vector.VectorNotations.
+Delimit Scope vector_scope with vector.
+Require Export List.
+Coercion Vector.to_list : Vector.t >-> list.
+
+Require Vector.
 Definition VariableT := Variables.U verse_type_system.
+Definition constant ty := const ty.
+Definition Declaration n := Vector.t (some (typeOf verse_type_system)) n.
+Definition Var (v : VariableT) k ty : v k ty -> some (typeOf verse_type_system)
+  := fun _ => existT _ k ty.
+
+Arguments Declaration [n].
+Arguments Var [v k ty].
+
 (* Require Export Verse.Types. *)
 (* Require Export Verse.Types.Internal. *)
 (* Require Export Verse.Word. *)
 (* Require Export Verse.Error. *)
 (* Require Export Verse.Syntax. *)
 (* Require Export Verse.PrettyPrint. *)
-(* Require Export Verse.Nibble. *)
-(* Require Export String. *)
+
+
 (* Require Export Nat. *)
-(* Require Vector. *)
-(* Export Vector.VectorNotations. *)
-(* Delimit Scope vector_scope with vector. *)
-(* Require Export List. *)
-(* Coercion Vector.to_list : Vector.t >-> list. *)
