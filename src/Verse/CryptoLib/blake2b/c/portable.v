bb70db473ae19cd828167f379d81f62f86a6530b
Piyush P Kurur <ppk@iitpkd.ac.in>
2020-03-17 12:08:11 +0530

    blake2b: last block function.

--- src/Verse/CryptoLib/blake2b/c/portable.v
+++ src/Verse/CryptoLib/blake2b/c/portable.v
@@ -33,16 +33,23 @@ End Config.
 Module Blake2b := Blake2 (Config).
 Require Import Verse.Target.C.
 
-Inductive name := verse_blake2b_c_portable.
-
+Inductive name := verse_blake2b_c_portable_iter |verse_blake2b_c_portable_last.
 Require Verse.CryptoLib.blake2.
 
 
 Module Allocation.
-  Axiom blockPtr : cvar (ptrToArray blake2.BLOCK_SIZE uint64_t).
+  Axiom blockPtr  : cvar (ptrToArray blake2.BLOCK_SIZE uint64_t).
   Axiom nBlocks  : cvar uint64_t.
   Axiom UpperRef LowerRef : cvar (array 1 uint64_t).
+
+  Axiom lastBlock : cvar (array blake2.BLOCK_SIZE uint64_t).
+  Axiom nBytes   : cvar uint64_t.
+  Axiom Upper Lower : cvar uint64_t.
+  Axiom f0 f1 : cvar uint64_t.
+
   Axiom hash     : cvar (ptrToArray blake2.HASH_SIZE uint64_t).
+
+
   Axiom h0 h1 h2 h3 h4 h5 h6 h7 : cvar uint64_t.
   Axiom w0 w1 w2 w3 w4 w5 w6 w7 w8 w9 w10 w11 w12 w13 w14 w15 : cvar uint64_t.
   Axiom v0 v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 v11 v12 v13 v14 v15 : cvar uint64_t.
@@ -52,7 +59,10 @@ End Allocation.
 Import Allocation.
 
 Definition params     := (- UpperRef , LowerRef , hash -).
+Definition paramsLast := (- lastBlock, nBytes, Upper, Lower, f0, f1, hash -).
+
 Definition locals     := (- h0 , h1 , h2 , h3 , h4 , h5 , h6 , h7 -).
+
 Definition registers  :=
   (- w0 , w1 , w2 , w3 , w4 , w5 , w6 , w7 , w8 , w9 , w10 , w11 , w12 , w13,  w14 ,  w15 ,
      v0 , v4 , v8 , v12,
@@ -62,9 +72,18 @@ Definition registers  :=
      C, LMSB, U, L
   -).
 
+Definition registersLast  :=
+  (- w0 , w1 , w2 , w3 , w4 , w5 , w6 , w7 , w8 , w9 , w10 , w11 , w12 , w13,  w14 ,  w15 ,
+     v0 , v4 , v8 , v12,
+     v1 , v5 , v9 , v13,
+     v2 , v6 , v10, v14,
+     v3 , v7 , v11, v15,
+     C, LMSB
+  -).
+
 
 Definition blake2bIter
-  := Compile.Iterator verse_blake2b_c_portable
+  := Compile.Iterator verse_blake2b_c_portable_iter
                       Blake2b.Block
                       Blake2b.paramIterator
                       Blake2b.stack
@@ -76,15 +95,26 @@ Definition blake2bIter
                       registers
                       Blake2b.Iterator.
 
-Require Import Verse.Error.
-Definition program : Compile.program := recover (blake2bIter).
 
+Definition blake2bLast
+  := Compile.Function verse_blake2b_c_portable_last
+                      Blake2b.paramLastBlock
+                      Blake2b.stack
+                      Blake2b.regLastBlock
+                      paramsLast
+                      locals
+                      registersLast
+                      Blake2b.LastBlockCompress.
+
+Require Import Verse.Error.
+Definition program   : Compile.program := recover (blake2bIter).
+Definition programLast : Compile.program := recover (blake2bLast).
 
 (*
 
 Require Import Verse.Print.
 Require Import Verse.Target.C.Pretty.
-Goal to_print program.
+Goal to_print programLast.
   print.
 Abort.
 
