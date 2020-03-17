110c06c9c392fa505d4b1d1e46faabf0300bb481
Piyush P Kurur <ppk@iitpkd.ac.in>
2020-03-17 12:08:27 +0530

    blake2s: last block function.

--- src/Verse/CryptoLib/blake2s/c/portable.v
+++ src/Verse/CryptoLib/blake2s/c/portable.v
@@ -35,25 +35,36 @@ End Config.
 Module Blake2s := Blake2 (Config).
 Require Import Verse.Target.C.
 
-Inductive name := verse_blake2s_c_portable.
+Inductive name := verse_blake2s_c_portable_iter | verse_blake2s_c_portable_last.
 
 Require Verse.CryptoLib.blake2.
 
 
 Module Allocation.
-  Axiom blockPtr : cvar (ptrToArray blake2.BLOCK_SIZE uint32_t).
+
+  Definition cword := uint32_t.
+
+  Axiom blockPtr : cvar (ptrToArray blake2.BLOCK_SIZE cword).
   Axiom nBlocks  : cvar uint64_t.
-  Axiom UpperRef LowerRef : cvar (array 1 uint32_t).
-  Axiom hash     : cvar (ptrToArray blake2.HASH_SIZE uint32_t).
-  Axiom h0 h1 h2 h3 h4 h5 h6 h7 : cvar uint32_t.
-  Axiom w0 w1 w2 w3 w4 w5 w6 w7 w8 w9 w10 w11 w12 w13 w14 w15 : cvar uint32_t.
-  Axiom v0 v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 v11 v12 v13 v14 v15 : cvar uint32_t.
-  Axiom C LMSB U L : cvar uint32_t.
+  Axiom UpperRef LowerRef : cvar (array 1 cword).
+
+  Axiom lastBlock : cvar (array blake2.BLOCK_SIZE cword).
+  Axiom nBytes   : cvar uint64_t.
+  Axiom Upper Lower : cvar cword.
+  Axiom f0 f1 : cvar cword.
+
+  Axiom hash     : cvar (ptrToArray blake2.HASH_SIZE cword).
+  Axiom h0 h1 h2 h3 h4 h5 h6 h7 : cvar cword.
+  Axiom w0 w1 w2 w3 w4 w5 w6 w7 w8 w9 w10 w11 w12 w13 w14 w15 : cvar cword.
+  Axiom v0 v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 v11 v12 v13 v14 v15 : cvar cword.
+  Axiom C LMSB U L : cvar cword.
 End Allocation.
 
 Import Allocation.
 
 Definition params     := (- UpperRef , LowerRef , hash -).
+Definition paramsLast := (- lastBlock, nBytes, Upper, Lower, f0, f1, hash -).
+
 Definition locals     := (- h0 , h1 , h2 , h3 , h4 , h5 , h6 , h7 -).
 Definition registers  :=
   (- w0 , w1 , w2 , w3 , w4 , w5 , w6 , w7 , w8 , w9 , w10 , w11 , w12 , w13,  w14 ,  w15 ,
@@ -64,9 +75,18 @@ Definition registers  :=
      C, LMSB, U, L
   -).
 
+Definition registersLast  :=
+  (- w0 , w1 , w2 , w3 , w4 , w5 , w6 , w7 , w8 , w9 , w10 , w11 , w12 , w13,  w14 ,  w15 ,
+     v0 , v4 , v8 , v12,
+     v1 , v5 , v9 , v13,
+     v2 , v6 , v10, v14,
+     v3 , v7 , v11, v15,
+     C, LMSB
+  -).
+
 
 Definition blake2sIter
-  := Compile.Iterator verse_blake2s_c_portable
+  := Compile.Iterator verse_blake2s_c_portable_iter
                       Blake2s.Block
                       Blake2s.paramIterator
                       Blake2s.stack
@@ -78,15 +98,25 @@ Definition blake2sIter
                       registers
                       Blake2s.Iterator.
 
+Definition blake2sLast
+  := Compile.Function verse_blake2s_c_portable_last
+                      Blake2s.paramLastBlock
+                      Blake2s.stack
+                      Blake2s.regLastBlock
+                      paramsLast
+                      locals
+                      registersLast
+                      Blake2s.LastBlockCompress.
+
 Require Import Verse.Error.
 Definition program : Compile.program := recover (blake2sIter).
-
+Definition programLast : Compile.program := recover (blake2sLast).
 
 (*
 
 Require Import Verse.Print.
 Require Import Verse.Target.C.Pretty.
-Goal to_print program.
+Goal to_print programLast.
   print.
 Abort.
 
