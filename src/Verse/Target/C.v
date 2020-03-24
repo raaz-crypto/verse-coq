75782b153a9f704d792fc9a180717411a31a4515
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-03-24 12:51:25 +0530

    target C now exposes the C pretty printing.

--- src/Verse/Target/C.v
+++ src/Verse/Target/C.v
@@ -1,4 +1,7 @@
-Require Export Verse.Target.C.Ast.
 Require Import Verse.Target.
 Require Import Verse.Target.C.CodeGen.
+
 Module Compile := Target.CodeGen (C.CodeGen.Config).
+
+Require Export Verse.Target.C.Ast.
+Require Export Verse.Target.C.Pretty.
