1b3a2be050c4d18dd967b4d6e5f435f3b2ce332a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-04-01 22:54:40 +0530

    better naming of modules.

--- src/Verse/FFI/Raaz/Target/C.v
+++ src/Verse/FFI/Raaz/Target/C.v
@@ -1,11 +1,11 @@
 Require Import Arith.
 Require Import Verse.TypeSystem.
 Require Import Verse.Language.Types.
-Require Import Verse.FFI.Raaz.Types.
+Require Import Verse.FFI.Raaz.
 
 
 Definition counterType := Word 64.
-Fixpoint translate {k} (ty : typeOf verse_type_system k) : Raaz.Types.type
+Fixpoint translate {k} (ty : typeOf verse_type_system k) : Raaz.type
   := match ty with
      | word n
        => match n with
@@ -26,20 +26,20 @@ Fixpoint translate {k} (ty : typeOf verse_type_system k) : Raaz.Types.type
      end.
 
 Require Import Verse.
-Definition fromDecl {n}(params : @Declaration n) : list Raaz.Types.type
+Definition fromDecl {n}(params : @Declaration n) : list Raaz.type
   := List.map (fun st => translate (projT2 st)) (Vector.to_list params).
 
 
 (** Generate the Haskell FFI stub for a straight line function *)
 Definition function {Name} (name : Name)
-                    {n : nat} (params : @Declaration n) : Raaz.Types.Foreign
+                    {n : nat} (params : @Declaration n) : Raaz.Foreign
   := ccall name (fromDecl params).
 
 (** Generate the Haskell FFI stub for an iterator *)
 Definition iterator
            {Name} (name : Name)
            (memty : typeOf verse_type_system memory)
-           {n : nat} (params : @Declaration n) : Raaz.Types.Foreign :=
+           {n : nat} (params : @Declaration n) : Raaz.Foreign :=
   let block   := translate memty in
   let args := fromDecl params in
   ccall name (block :: counterType :: args).
