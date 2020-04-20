71c982f4d3451568faa565a230b9501239c9378b
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-04-20 12:09:40 +0530

    rename Machine type to context.

--- src/Verse/Machine.v
+++ src/Verse/Machine.v
@@ -9,14 +9,13 @@ Import EqNotations.
 One way to give semantics to verse is through an abstract state
 transformation machine, a machine with infinitely many registers, one
 for each natural number. Each of these registers have an associated
-type and only hold values of that type. What we really have therefore
-is a *family* of abstract machines parameterised on the types that we
-assign to these registers. The type given below is the type of the
-abstract machine.
+type and only hold values of that type. This determines the typing
+context in which we need to interpret the execution of the
+machine. We define the type of contexts below.
 
  *)
 
-Definition type := nat -> Type.
+Definition context := nat -> Type.
 
 (** ** The state of the machine.
 
@@ -25,7 +24,7 @@ the machine is an assignment of values to the registers consistent with [fam].
 
 *)
 
-Definition state (typ : type) := forall reg, typ reg.
+Definition state (cxt : context) := forall reg, cxt reg.
 
 (** An abstract instruction for the machine is just a state
 transformation. Being a monoid, a program is composed of instructions
@@ -33,15 +32,15 @@ and hence is also a state transformation.
 
 *)
 
-Definition instruction (typ : type) := state typ -> state typ.
-Definition program (typ : type)     := instruction typ.
+Definition instruction (cxt : context) := state cxt -> state cxt.
+Definition program     (cxt : context) := instruction cxt.
 
 
 (**  One such instruction is the update of a register with an appropriate value
 
 *)
 
-Definition modify {typ : type} reg (v : typ reg) : instruction typ
+Definition modify {cxt : context} reg (v : cxt reg) : instruction cxt
     := fun st regp => match Nat.eq_dec reg regp with
                    | left pf => rew pf in v
                    | _       => st regp
@@ -66,10 +65,10 @@ Definition abstract_type_system : typeSystem
 
 (** The variables the register set captured by the following inductive type.
  *)
-Inductive register (typ : type) : Variables.U abstract_type_system :=
-| R : forall k idx, register typ k (typ idx).
+Inductive register (cxt : context) : Variables.U abstract_type_system :=
+| R : forall k idx, register cxt k (cxt idx).
 
-Arguments R [typ k].
+Arguments R [cxt k].
 
 Require Verse.Ast.
 
@@ -78,39 +77,39 @@ Import Vector.VectorNotations.
 Module Internals.
 
   Section MachineType.
-    Variable typ : type.
+    Variable cxt: context.
 
-    Definition expr  T := Ast.expr  (register typ) T.
-    Definition lexpr T := Ast.lexpr (register typ) T.
+    Definition expr  T := Ast.expr  (register cxt) T.
+    Definition lexpr T := Ast.lexpr (register cxt) T.
 
     Definition lookup {k} {T : typeOf abstract_type_system k}
-               (st : state typ)(reg : register typ k T) : T
+               (st : state cxt)(reg : register cxt k T) : T
       := match reg with
          | R idx => st idx
          end.
 
     Definition updateReg {k} {T : typeOf abstract_type_system k}
-               (reg : register typ k T) (v : T) (st : state typ)
-      : state typ
-      := match reg in (register _ k0 t) return (t -> state typ) with
-         | R idx => fun v0 : typ idx => modify idx v0 st
+               (reg : register cxt k T) (v : T) (st : state cxt)
+      : state cxt
+      := match reg in (register _ k0 t) return (t -> state cxt) with
+         | R idx => fun v0 : cxt idx => modify idx v0 st
          end v.
 
-    Definition leval {T} (l : lexpr T) (st : state typ) : T
+    Definition leval {T} (l : lexpr T) (st : state cxt) : T
       := match l with
          | Ast.var reg => lookup st reg
          | Ast.deref v idx  => Vector.nth_order (lookup st v) (proj2_sig idx)
          end.
 
-    Fixpoint eval {T} (st : state typ) (e : expr T) :  T
+    Fixpoint eval {T} (st : state cxt) (e : expr T) :  T
       := match e with
          | Ast.cval c => c
          | Ast.valueOf lv => leval lv st
          | Ast.app o args => o (Vector.map (eval st) args)
          end.
 
-    Definition assign {T} (l : lexpr T) (e : expr T)(st : state typ) : state typ
-      := match l in Ast.lexpr _ T0 return  T0 -> state typ with
+    Definition assign {T} (l : lexpr T) (e : expr T)(st : state cxt) : state cxt
+      := match l in Ast.lexpr _ T0 return  T0 -> state cxt with
          | Ast.var reg  => fun v => updateReg reg v st
          | Ast.deref a idx => fun v => let arr := lookup st a in
                                    let arrp := Vector.replace_order arr (proj2_sig idx) v in
@@ -119,21 +118,21 @@ Module Internals.
 
     Definition update {T} (l : lexpr T){arity}
                (o : operator abstract_type_system T (S arity))
-               (args : Vector.t (expr T) arity)(st : state typ) : state typ
+               (args : Vector.t (expr T) arity)(st : state cxt) : state cxt
       := let rhs := Ast.app o (Ast.valueOf l :: args) in
          assign l rhs st.
 
-    Definition move {T}(l : lexpr T)(r : register typ direct T) :=
+    Definition move {T}(l : lexpr T)(r : register cxt direct T) :=
       assign l (Ast.valueOf (Ast.var r)).
 
-    Definition denoteInst {T}(inst : Ast.instruction (register typ)  T) : instruction typ
+    Definition denoteInst {T}(inst : Ast.instruction (register cxt)  T) : instruction cxt
       := match inst with
          | Ast.assign l e => assign l e
          | Ast.moveTo l r => move   l r
          | Ast.update l o e => update l o e
          | Ast.clobber _     => fun x => x
          end.
-    Definition denoteStmt (stmt : Ast.statement (register typ)) : instruction typ
+    Definition denoteStmt (stmt : Ast.statement (register cxt)) : instruction cxt
       := denoteInst (projT2 stmt).
   End MachineType.
 
@@ -141,8 +140,8 @@ Module Internals.
 End Internals.
 
 Require Import Verse.Monoid.Semantics.
-Definition machine_semantics (typ : type) : semantics (instruction typ)
+Definition machine_semantics (cxt : context) : semantics (instruction cxt)
   := {| types     := abstract_type_system;
-        variables := register typ;
-        denote    := Internals.denoteStmt typ
+        variables := register cxt;
+        denote    := Internals.denoteStmt cxt
       |}.
