44f98c32516b41564111c1a37e9261a61336b68f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-05-01 15:42:05 +0530

    a much more brutal crush for bitvector proofs.

--- src/Verse/BitVector/Facts.v
+++ src/Verse/BitVector/Facts.v
@@ -5,8 +5,38 @@ Require Import Verse.BitVector.
 
 Hint Unfold BVshiftR BVshiftL : bitvector.
 
+Lemma vector_0_nil : forall A (vec : Vector.t A 0), vec = [].
+  intro A.
+  exact (Vector.case0 (fun v => v = []) eq_refl).
+Qed.
+Ltac crush := repeat (simpl; eauto with bitvector;
+                      match goal with
+                      | [ |- bool -> _ ] => intro
+                      | [b : bool |- _ ] => destruct b
+                      | [ v : Bvector 0     |- _ ] => rewrite (vector_0_nil _ v)
+                      | [ v : Bvector (S _) |- _ ]
+                        => rewrite (Vector.eta v);
+                          let h := fresh "h" in
+                          let t := fresh "t" in
+                          (generalize (Vector.hd v) as h;
+                           generalize (Vector.tl v) as tl; intros h t)
 
-Ltac crush := repeat (simpl; eauto with bitvector).
+                      | [ v : _ |- context[Vector.hd ?v] ] => generalize (Vector.hd v)
+                      | _ => idtac
+                      end).
+
+(** A more powerful induction
+
+1. Apply induction on the given variable
+
+2. Base case is crushed
+
+3. The induction step is first softened by a crush then a rewrite by the Inductive hypothesis
+   followed by crush.
+
+
+*)
+
 Ltac induct_on n :=
   let m := fresh "m" in
   let IHm := fresh "IHm" in
