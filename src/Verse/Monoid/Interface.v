30161dd47c28f9c810d6cdb5cf6c2991d802fecc
dangabhi <ahdang@iitk.ac.in>
2020-10-12 22:19:37 +0530

    machine specs divorced from interface

--- src/Verse/Monoid/Interface.v
+++ src/Verse/Monoid/Interface.v
@@ -1,18 +1,27 @@
+Require Import SetoidClass.
 Require Import Verse.Monoid.
 Require Import Verse.TypeSystem.
 
-Class Interface {ltypes}
-                (v : Variables.U ltypes)
+(* TODO - mtypes can be taken in again *)
+Record mSpecs ltypes mtypes
   := {
-      mtypes       : typeSystem;
-      variables    : Variables.U mtypes;
-      typeCompiler : TypeSystem.compiler ltypes mtypes;
+      mvariables   : Variables.U mtypes;
+      mtypeCompiler : TypeSystem.compiler ltypes mtypes;
+    }.
 
+Arguments mvariables [ltypes mtypes].
+Arguments mtypeCompiler [ltypes mtypes].
+
+Class Interface {ltypes mtypes}
+      (v : Variables.U ltypes)
+      (S : mSpecs ltypes mtypes)
+  := {
       Var : forall {k} {ty : typeOf ltypes k},
-            v _ ty -> Variables.Universe.embed variables
-                                               (typeTrans typeCompiler ty)
-            (* This cannot use Universe.inject as if the typeCompiler
-               is a true compiler, a Var map would not be possible at
-               all!
-            *)
+        v _ ty -> Variables.Universe.embed (mvariables S)
+                                           (typeTrans (mtypeCompiler S) ty)
+
+      (* This cannot use Universe.inject as if the typeCompiler
+         is a true compiler, a Var map would not be possible at
+         all!
+       *)
     }.
