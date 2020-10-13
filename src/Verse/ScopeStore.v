4e4ca3ea70577910ca384b8028c9810607a34bfd
dangabhi <ahdang@iitk.ac.in>
2020-10-13 20:16:14 +0530

    store with scopeVar

---
+++ src/Verse/ScopeStore.v
@@ -0,0 +1,109 @@
+Require Import Verse.TypeSystem.
+Require Import Verse.Language.
+Require Verse.Scope.
+Require Import Verse.AbstractMachine.
+Require Import Verse.Monoid.Semantics.
+
+Require Import Eqdep_dec.
+Require Import Equality.
+
+Require Import Vector.
+Import VectorNotations.
+
+Set Implicit Arguments.
+
+Section ScopeStore.
+
+  Variable ts : typeSystem.
+  Variable v : Variables.U ts.
+  Variable tyD : typeDenote ts.
+
+  Local Definition tyd  := typeTrans tyD.
+  Arguments tyd [k].
+
+  Definition type := typeOf ts.
+
+  Definition scstr n (sc : Scope.type ts n)
+    := Scope.allocation tyd sc.
+
+  Fixpoint frmStore
+           n [sc : Scope.type ts n] (s : Store (scstr sc))
+           k [ty : type k] (x : Scope.scopeVar sc _ ty)
+    : tyd ty
+    :=
+      match x in Scope.scopeVar sc0 _ ty0 return scstr sc0
+                                                 -> (tyd ty0 : Type) with
+      | Scope.headVar    => fun s0 => let '(vx, _) := s0 in vx
+      | Scope.restVar rx => fun s0 => let '(_, st) := s0 in frmStore {| store := st |} rx
+      end store.
+
+Fixpoint wrtStore
+         n (sc : Scope.type ts n)
+         k [ty : type k] (var : Scope.scopeVar sc _ ty)
+  : (tyd ty -> tyd ty) ->
+    Store (scstr sc) -> Store (scstr sc) :=
+  match var as var0 in Scope.scopeVar sc0 _ ty0 return
+        (tyd ty0 -> tyd ty0)
+        -> Store (scstr sc0) -> Store (scstr sc0)
+  with
+  | Scope.headVar    => fun f s => let '(vx, st) := store in {| store := (f vx, st) |}
+  | Scope.restVar rx => fun f s => let '(vx, st) := store in {| store := (vx, store (Store := wrtStore rx f {| store := st |})) |}
+  end.
+
+Axiom frmWrt :
+  forall n (sc : Scope.type ts n)
+           (st : Store (scstr sc))
+           k (ty : type k) (var : Scope.scopeVar sc _ ty) f
+           k' (ty' : type k') (v' : Scope.scopeVar sc _ ty'),
+    ( ~ eq_dep2 var v'-> frmStore (wrtStore var f st) v' = frmStore st v')
+    /\
+    frmStore (wrtStore var f st) var = f (frmStore st var).
+
+Instance scopeStore
+           n (sc : Scope.type ts n)
+  : State (Scope.scopeVar sc) tyD :=
+  {| str   := _;
+
+     val := @frmStore n sc;
+
+     storeUpdate := @wrtStore _ sc;
+
+     evalUpdate := @frmWrt _ sc;
+  |}.
+
+End ScopeStore.
+
+Require Import Vector.
+Import VectorNotations.
+Require Import Verse.Language.Types.
+
+Section CodeGen.
+
+  Variable n : nat.
+  Variable sc : Scope.type verse_type_system n.
+
+  Variable tyD : typeDenote verse_type_system.
+
+  Definition asc
+    := Vector.map (fun sty => let 'existT _ _ ty := sty in
+                              existT _ _ (typeTrans tyD ty))
+                  sc.
+
+  Variable ac : forall v, Scope.scoped v sc (AnnotatedCode v tyD).
+
+  Definition c := Scope.fillScoped ac (scopeStore _ _).
+
+  Definition cp := linesDenote _ _ store_semantics c.
+
+End CodeGen.
+
+(* Extracting Prop object from annotated code *)
+
+Ltac getProp func
+  := (let level0 := constr:(Scope.Cookup.specialise func) in
+      let level0break := (eval hnf in (Scope.inferNesting level0)) in
+      let pvs := constr:(fst level0break) in
+      let level1 := constr:(snd level0break) in
+      let lvs := (eval hnf in (fst (Scope.inferNesting level1))) in
+      exact (forall (st : str), snd (cp (pvs ++ lvs) _ func)
+                                    ({| store := st |}, {| store := st |}))).
