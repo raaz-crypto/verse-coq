89096f684bf2e4d233f619d829fa183d2327f3ab
Piyush P Kurur <ppk@cse.iitk.ac.in>
2021-01-31 14:58:56 +0530

    monad laws.

--- src/Verse/Monad.v
+++ src/Verse/Monad.v
@@ -46,6 +46,32 @@ Notation "'do' pat <- y ';;' z"
   := (y >>= fun (x : _) => match x with | pat => z end)
        (at level 61, pat pattern, y at next level, only parsing, right associativity).
 
+(** The monadic laws.
+
+Instances that do not satisfies the monadic laws are problematic and
+can fail the intuition behind the do notation. It is therefore good to
+prove the monadic laws.
+
+*)
+Section Laws.
+  Context {T}(monad : Monad T).
+  Definition pure_id_l : Prop := forall A B (x : A)(f : A -> T B), pure x >>= f = f x.
+  Definition pure_id_r : Prop := forall A B (x : A)(f : A -> T B), pure x >>= f = f x.
+  Definition bind_assoc : Prop := forall A B C (ta : T A)(f : A -> T B)(g : B -> T C),
+      (ta >>= f) >>= g  = ta >>= fun x => f x >>= g.
+
+  Definition monad_laws : Prop := pure_id_l /\ pure_id_r /\ bind_assoc.
+End Laws.
+
+Ltac crush_monad_laws :=
+  unfold monad_laws;  intuition;
+  unfold pure_id_l; unfold pure_id_r; unfold bind_assoc;
+  intros;
+  match goal with
+  | [ ta : ?T _ |- _ ] => destruct ta; trivial
+  | _ => trivial
+  end.
+
 
 Instance opt_monad : Monad option :=
   let bnd  := fun A B (x : option A)(f : A -> option B) =>
