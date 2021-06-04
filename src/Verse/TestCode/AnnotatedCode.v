62ce66137015af6781639b3a9743d78626dd4bbd
dangabhi <ahdang@iitk.ac.in>
2021-06-04 13:53:56 +0530

    sample code updated

--- src/Verse/TestCode/AnnotatedCode.v
+++ src/Verse/TestCode/AnnotatedCode.v
@@ -3,26 +3,32 @@ Require Import Verse.Machine.BitVector.
 Require Import Verse.AbstractMachine.
 Require Import Verse.Monoid.
 Require Import Verse.ScopeStore.
+Require Import Verse.AnnotatedCode.
 
 Require Import Verse.
 
+Open Scope annotation_scope.
+
 Set Implicit Arguments.
 
-Section Code.
+ Section Code.
 
   Variable v : VariableT.
 
   Variable A B : v Word8.
 
-  Definition test : AnnotatedCode v bvDenote.
-    annotated_verse
-      [
-        A ::= B;
-        ASSERT (VAL A = OLDVAL A);
-        A ::= B;
-        ASSERT (OLDVAL B = BVplus (VAL A) BVones);
-        A ::= B
-      ]%code.
+  Definition test : AnnotatedCode v bvDenote noRels.
+    verse (
+          CODE [ A ::= B;
+                 B ::= 5
+               ]
+          ++
+          ANNOT [ A = (OLD B) ]
+          ++
+          CODE [ B ::= A; A ::= 6 ]
+          ++
+          ANNOT [ B = (OLD B) ]
+      )%list%verse.
   Defined.
 
 End Code.
@@ -32,5 +38,7 @@ Definition toProve : Prop.
 Defined.
 
 Definition proof : toProve.
+
   simplify.
+
 Abort.
