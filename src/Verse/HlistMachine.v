530747e43f561cdbe00a34a2ba99074581cc3926
Abhishek Dang <ahdang@iitk.ac.in>
2022-03-01 20:16:24 +0530

    prop extraction code for HlistMachine

--- src/Verse/HlistMachine.v
+++ src/Verse/HlistMachine.v
@@ -16,6 +16,11 @@ Section Hlist.
 
   Definition memV : Variables.U ts := fun ty => member ty sc.
 
+  (* Generic scoped code instantiated with `memV` *)
+  Definition fillMemV [CODE : Variables.U ts -> Type]
+             (genC : forall v, scoped v sc (CODE v)) :=
+    uncurry (genC memV) (all_membership sc).
+
   Instance HlistMem : State memV tyD.
   refine {| str         := memory tyd sc;
             val         := fun m _ v => index v m;
@@ -25,6 +30,67 @@ Section Hlist.
   constructor.
   apply update_other_index.
   apply updated_index.
-  Qed.
+  Defined.
 
 End Hlist.
+
+Arguments fillMemV [ts sc CODE].
+
+Require Import Verse.AnnotatedCode.
+Require Import Vector.
+Import VectorNotations.
+Require Import Verse.Language.Types.
+
+Section CodeGen.
+
+  Variable n : nat.
+  Variable sc : Scope.type verse_type_system.
+
+  Variable tyD : typeDenote verse_type_system.
+  Variable Rels : forall (ty : typeOf verse_type_system direct),
+      Rel tyD ty -> Prop
+  .
+
+  Variable ac : forall v, Scope.scoped v sc (AnnotatedCode tyD Rels v).
+
+  Definition cp := interpret (denote (fillMemV ac)).
+
+  (* We allow `getProp` to take a precondition to prefix to the
+     extracted Prop. This is never exposed to the user, but is used in
+     the CoarseAxiom to provide the proof of the procedure call
+     specification to the main body proof.
+   *)
+
+  Definition getProp (pc : _ -> Prop)
+             (ml : @mline _ (memV sc) tyD)
+    := forall (st : str), pc st
+                          ->
+                          snd (ml (HlistMem _ _))
+                              (st, st).
+
+  Definition tpt := getProp (fun _ => True) cp.
+(*
+  Definition getProp (ml : @mline _ (Scope.scopeVar sc) tyD)
+    := forall (st : str), snd (ml (scopeStore _ _))
+                              ({| store := st |}, {| store := st |}).
+
+  Definition tpt := getProp cp.
+*)
+End CodeGen.
+
+Global Hint Unfold tpt : Wrapper.
+Global Hint Unfold cp  : Wrapper.
+
+Arguments cp sc [tyD].
+Arguments getProp [sc tyD].
+Arguments tpt sc [tyD Rels].
+
+(* Extracting Prop object from annotated code *)
+
+Ltac getProp func
+  := (let level0 := constr:(Scope.Cookup.specialise func) in
+      let level0break := (eval hnf in (Scope.inferNesting level0)) in
+      let pvs := constr:(fst level0break) in
+      let level1 := constr:(snd level0break) in
+      let lvs := (eval hnf in (fst (Scope.inferNesting level1))) in
+      exact (tpt (pvs ++ lvs)%list func)).
