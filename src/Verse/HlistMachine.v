703074ab8472c6ef9e7bfa82f6ed4e21960c0786
Abhishek Dang <ahdang@iitk.ac.in>
2022-03-01 14:28:17 +0530

    hlist state added

---
+++ src/Verse/HlistMachine.v
@@ -0,0 +1,30 @@
+(* begin hide *)
+Require Import Verse.TypeSystem.
+Require Verse.Scope.
+Require Import Verse.Utils.hlist.
+Require Import Verse.AbstractMachine.
+Require Import Verse.Abstract.Machine.
+(* end hide *)
+
+Section Hlist.
+
+  Context [ts : typeSystem] (sc : Scope.type ts).
+
+  Variable tyD : typeDenote ts.
+
+  Local Definition tyd ty := typeTrans tyD (projT2 ty).
+
+  Definition v : Variables.U ts := fun ty => member ty sc.
+
+  Instance HlistMem : State v tyD.
+  refine {| str         := memory tyd sc;
+            val         := fun m _ v => index v m;
+            storeUpdate := fun _ v f m => update v m (f (index v m));
+            evalUpdate  := _;
+         |}.
+  constructor.
+  apply update_other_index.
+  apply updated_index.
+  Qed.
+
+End Hlist.
