67bd93c5c8399a2d1abd730723b3d244bee17306
Piyush P Kurur <ppk@iitpkd.ac.in>
2022-06-20 13:11:07 +0530

    Load and store.

--- src/Verse/CryptoLib/curve25519/c/field.v
+++ src/Verse/CryptoLib/curve25519/c/field.v
@@ -27,7 +27,6 @@ There are two possible representations of elements in this field
   themselves can be considered as aᵢ + bᵢ 2²⁶.
 
 
-
 The packed representation is the standard representation and is in
 particular used to storing and transmitting values of the latter and
 is thus canonical.  The computational representation should be treated
@@ -36,27 +35,169 @@ the implementation of the field operation efficient.
 
  *)
 
-Definition pow2 (i : nat) :=
+
+Definition pos (i : nat) :=
   let j := i / 2 in
   let k := i mod 2 in
   j * 51 + k * 26.
 
+Definition len (i : nat) := pos (S i) - pos i.
 
-(** The bit range that goes into the ith limb vᵢ and the number of bits that it should hold. *)
+Definition nLimbs := 10.
+Definition nWord  := 4.
 
-Definition bitrange (i : nat) := (pow2 i, pow2 (i + 1) ).
-Definition nbits   (i : nat) := let (l,h) := bitrange i in
-                              h - l.
+Definition foreachLimb {A : Type}(f : forall i, i < nLimbs -> list A) : list A := iterate f.
+Definition foreachWord {A : Type}(f : forall i, i < nWord  -> list A) : list A := iterate f.
 
-Definition bitSizes := iterate (fun i (_ : i < 10) =>[nbits i]).
-
+Definition Packed := Array 4 littleE Word64.
+(** Field element *)
+Definition fe (v : VariableT) := Vector.t (v of type Word64) 10.
 
 (* begin hide *)
 (* NOTE: These are inline tests *)
-
-(* Make sure that all the 255 bits are distributed some where.
-Goal List.fold_left Nat.add bitSizes 0 = 255.
+Definition bitSizes := foreachLimb (fun i _  => [len i] ).
+(* Make sure that all the 255 bits are distributed some where among the limbs. *)
+Goal List.list_sum bitSizes = 255.
   trivial.
 Qed.
+
+(* end hide *)
+
+Module Internal.
+
+  Inductive Transfer :=
+  | FullT  : nat -> nat -> Transfer
+  | LowerT : nat -> Transfer
+  | UpperT : nat -> Transfer.
+
+
+  Definition transferSize (t : Transfer) :=
+    match t with
+    | FullT _ l | LowerT l | UpperT l => l
+    end.
+
+  Section Transfer.
+    (* The entire limb comes from this word *)
+
+    Context (i j : nat).
+
+    Definition this := i * 64.
+    Definition next := S i * 64.
+
+
+    Definition thisL := pos j.
+    Definition nextL := pos (S j).
+    Definition endL  := nextL - 1.
+
+    Definition inRangeB m : bool :=
+       (this <=? m) && (m <? next).
+
+    Definition fullTB  : bool := inRangeB thisL && inRangeB endL.
+    Definition lowerTB : bool := inRangeB thisL && negb (inRangeB endL).
+    Definition upperTB : bool := negb (inRangeB thisL) && inRangeB endL.
+
+    Definition transfer : option Transfer :=
+      (if inRangeB thisL && inRangeB endL then Some (FullT (thisL - this) (len j))
+       else if inRangeB thisL then Some (LowerT (next - thisL))
+            else if inRangeB endL then Some (UpperT (nextL - this))
+                 else None)%bool.
+
+    Definition toList {A}( oa : option A ) : list A :=
+      match oa with
+      | Some x => [x]
+      | None   => []
+      end.
+  End Transfer.
+
+  (* begin hide *)
+  Definition allLoads (i : nat) := foreachLimb (fun j _  => toList (transfer i j)).
+
+  Definition trSizes (i : nat) := List.list_sum (List.map transferSize (allLoads i)).
+
+  Goal trSizes 0 = 64 /\ trSizes 1 = 64 /\
+    trSizes 2 = 64.
+    compute. intuition.
+  Qed.
+
+  Goal List.list_sum (foreachWord (fun i _ => [trSizes i])) = 255.
+    compute.
+    trivial.
+  Qed.
+  (* end hide *)
+
+  Section Load.
+    Context {progvar : VariableT}.
+    Context (packed : progvar of type Packed).
+    Context (temp : progvar of type Word64).
+    Context (x : fe progvar).
+
+    Program Definition load (i j : nat)(_ : j < nLimbs) : code progvar :=
+      let trTo tr := match tr with
+                     | FullT p l => [verse| x[j] := `bitsAt p l temp` |]
+                     | LowerT n  => [verse| x[j] := `toTopBits n temp` |]
+                     | UpperT n  => let lBits := len j - n in
+                                   [verse| x[j] |= `keepOnlyLower n temp` ≪ lBits |]
+                     end in
+      match transfer i j  with
+      | Some tr => [trTo tr]
+      | None => []
+      end.
+
+    Program Definition store (i j : nat)(_ : j < nLimbs) : code progvar :=
+      let xj := [verse| x[j] |] in
+      let trTo tr := match tr with
+                     | FullT p l => if p =? 0 then [verse| temp := xj |]
+                                   else [verse| temp |= `bitsTo p l xj` |]
+                     | LowerT n => [verse| temp |= x[j] ≪ `64 - n`  |]
+                     | UpperT n => [verse| temp := `toTopBits n xj` |]
+                     end
+      in match transfer i j with
+         | Some tr => [trTo tr]
+         | None    => []
+         end.
+
+    Program Definition loadAll : code progvar := foreachWord (fun i _ =>
+                                                                [code| temp := packed [ i ] |] ++
+                                                                  foreachLimb (load i)
+                                                   )%list.
+    Program Definition storeAll : code progvar := foreachWord (fun i _ =>
+                                                                 foreachLimb (store i) ++
+                                                                   [code| packed[i] := temp |]
+                                                    )%list.
+  End Load.
+
+
+End Internal.
+
+(* begin hide *)
+
+
+Require Import Verse.Print.
+Require Import Verse.Target.C.Pretty.
+
+
+Axiom MyVar :VariableT.
+Axiom P : MyVar of type Packed.
+Axiom T : MyVar of type Word64.
+Axiom X : fe MyVar.
+Goal to_print (Internal.loadAll P T X ).
+  unfold Internal.loadAll;
+  unfold foreachWord;
+  unfold iterate;
+    unfold foreach;
+  simpl;
+    unfold bitsAt; unfold len; simpl;
+    unfold keepOnlyLower; simpl;
+    unfold keepAt; simpl.
+Abort.
+
+Goal to_print (Internal.storeAll P T X).
+  unfold Internal.storeAll;
+  unfold foreachWord;
+  unfold iterate;
+    unfold foreach;
+  simpl ; unfold len; simpl;
+  unfold bitsAt; unfold len; simpl.
+Abort.
 
 (* end hide *)
