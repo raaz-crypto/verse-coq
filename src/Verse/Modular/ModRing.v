01b81fd134fc82200827f369e755b85a0a291877
Piyush P Kurur <ppk@iitpkd.ac.in>
2022-07-13 11:41:01 +0530

    setoid instance for ModRing.

--- src/Verse/Modular/ModRing.v
+++ src/Verse/Modular/ModRing.v
@@ -1,6 +1,6 @@
 Require Import NArith.
 Require Import Verse.Modular.Equation.
-
+Require Import SetoidClass.
 Inductive Zmod (M : positive) :=
 | of_N : N -> Zmod M.
 
@@ -11,7 +11,29 @@ Definition to_N {M}(z : Zmod M) :=
   | of_N x => x mod (Npos M)
   end%N.
 
-Definition eqZmod {M} (x y : Zmod M) : Prop := to_N x ≡ to_N y [mod Npos M].
+Definition eqZmod {M} : relation (Zmod M) := fun x y => to_N x ≡ to_N y [mod Npos M].
+
+Lemma eqZmod_refl M : reflexive (Zmod M) eqZmod.
+  intros x;
+  reflexivity.
+Qed.
+
+Lemma eqZmod_sym M : symmetric (Zmod M) eqZmod.
+  intros x y;
+    symmetry; trivial.
+Qed.
+
+Lemma eqZmod_trans M : transitive (Zmod M) eqZmod.
+  intros x y z; unfold eqZmod.
+    transitivity (to_N y); trivial.
+Qed.
+
+
+
+Add Parametric Relation M : (Zmod M) eqZmod
+    reflexivity proved by (eqZmod_refl M)
+    symmetry proved by (eqZmod_sym M)
+    transitivity proved by (eqZmod_trans M) as zmod_rel.
 
 Definition add {M} (x y : Zmod M) : Zmod M := of_N (addMod (Npos M) (to_N x) (to_N y)).
 Definition mul {M} (x y : Zmod M) : Zmod M := of_N (mulMod (Npos M) (to_N x) (to_N y)).
@@ -25,3 +47,4 @@ Require Import setoid_ring.Algebra_syntax.
 #[export] Instance mul_Zmod M  : Multiplication  := @mul M.
 #[export] Instance sub_Zmod M  : Subtraction (Zmod M) := minus.
 #[export] Instance opp_Zmod M  : Opposite (Zmod M)   := opp.
+#[export] Instance setoid_Zmod M : Setoid (Zmod M)   := {| equiv := @eqZmod M; |}.
