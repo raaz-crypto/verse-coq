cbc6ed874348af4f22d6c884f2a9d1a85f2d98e0
Abhishek Dang <ahdang@iitk.ac.in>
2022-07-16 15:31:43 +0530

    duplicate notations removed

--- src/Verse/Language/Pretty.v
+++ src/Verse/Language/Pretty.v
@@ -17,7 +17,6 @@ syntax of these code values palatable to the user.
 Require Import NArith.
 Require Import Nat.
 Require Import Verse.Ast.
-Require Import Verse.AnnotatedCode.
 Require Import Verse.Language.Types.
 Require Import Verse.TypeSystem.
 Require        Vector.
@@ -254,30 +253,6 @@ Notation "'MOVE' B 'to' A [ N ]"
   := (existT _ _ (moveTo (deref A (exist _ (N%nat) _)) B)) (in custom verse at level 200, A ident).
  *)
 
-(* We overload notations for `instruction` and `line` *)
-Declare Scope code_scope.
-Delimit Scope code_scope with code.
-
-Notation "A ::= E" := (inst (assignStmt A E))  (at level 70) : code_scope.
-Notation "A <- B" := (inst (moveStmt A B)) (at level 70) : code_scope.
-Notation "A ::=+ B" := (inst (binOpUpdate plus A B)) (at level 70) : code_scope.
-Notation "A ::=- B" := (inst (binOpUpdate minus A B)) (at level 70) : code_scope.
-Notation "A ::=* B" := (inst (binOpUpdate mul A B)) (at level 70) : code_scope.
-Notation "A ::=/ B" := (inst (binOpUpdate quot A B)) (at level 70) : code_scope.
-Notation "A ::=% B" := (inst (binOpUpdate rem A B)) (at level 70) : code_scope.
-Notation "A ::=| B" := (inst (binOpUpdate bitOr A B)) (at level 70) : code_scope.
-Notation "A ::=& B"  := (inst (binOpUpdate bitAnd A B)) (at level 70) : code_scope.
-Notation "A ::=x B"  := (inst (binOpUpdate bitXor A B)) (at level 70, only parsing) : code_scope.
-Notation "A ::=⊕ B" := (inst (binOpUpdate bitXor A B)) (at level 70) : code_scope.
-
-Notation "A ::=<< N"   := (inst (uniOpUpdate (shiftL N) A))   (at level 70) : code_scope.
-Notation "A ::=>> N"   := (inst (uniOpUpdate (shiftR N) A))   (at level 70) : code_scope.
-Notation "A ::=<<< N"  := (inst (uniOpUpdate (rotL N)   A))   (at level 70) : code_scope.
-Notation "A ::=>>> N"  := (inst (uniOpUpdate (rotR N)   A))   (at level 70) : code_scope.
-Notation "'CLOBBER' A" := (inst (existT _ _ (clobber A)))     (at level 70) : code_scope.
-
-Notation "'MOVE' B 'TO' A [- N -]"
-  := (inst (existT _ _ (moveTo (deref A (exist _ (N%nat) _)) B))) (at level 200, A name) : code_scope.
 
 (** * The verse tactic.
 
