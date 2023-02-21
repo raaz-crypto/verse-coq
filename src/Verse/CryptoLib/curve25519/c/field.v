6fc4d1e6c56732e05b1ba1f89a77cb4f11127bb1
Piyush P Kurur <ppk@iitpkd.ac.in>
2023-02-21 11:40:02 +0530

    carry propagation with assignment.

--- src/Verse/CryptoLib/curve25519/c/field.v
+++ src/Verse/CryptoLib/curve25519/c/field.v
@@ -436,19 +436,34 @@ next limb (cycling when we reach the last limb *)
 Section CarryPropagation.
 
   Context {progvar : VariableT}.
-  Variable limb    : feVar progvar.
 
-  Program Definition carryFrom (i : nat)(_ : i < nLimbs) :=
-    if (i =? 9) then [verse| `19` * (limb[9] ≫ `len 9`) |]
-    else [verse| limb[ i ]  ≫ `len i` |].
+  Section Propagate.
+    Context (limb : feVar progvar).
+    Program Definition carryFrom (i : nat)(_ : i < nLimbs) :=
+      if (i =? 9) then [verse| `19` * (limb[9] ≫ `len 9`) |]
+      else [verse| limb[ i ]  ≫ `len i` |].
 
 
-  Definition propagateTo (i : nat)`(i < nLimbs) : code progvar.
-    verse [code| limb[i] += carryFrom[ (i + nLimbs - 1) mod nLimbs ] |].
-  Defined.
+    Definition propagateTo (i : nat)`(i < nLimbs) : Repeat (statement progvar).
+      verse [code| limb[i] += carryFrom[ (i + nLimbs - 1) mod nLimbs ] |].
+    Defined.
 
-  (* We perform a full cycle of propagation by starting at the highest limb *)
-  Definition propagate := foreachLimb propagateTo.
+    (* We perform a full cycle of propagation by starting at the highest limb *)
+    Definition propagate := foreachLimb propagateTo.
+
+  End Propagate.
+
+  Section AssignPropagate.
+    Context (Dest Src : feVar progvar).
+
+    Definition carrySrc := carryFrom Src.
+    Definition propagateAssignTo (i : nat)`(i < nLimbs) : code progvar.
+      verse [code| Dest[i] := Src [i] + carrySrc[ (i + nLimbs - 1) mod nLimbs ] |].
+    Defined.
+
+    Definition propagateAssign := foreachLimb propagateAssignTo.
+
+  End AssignPropagate.
 
 End CarryPropagation.
 
