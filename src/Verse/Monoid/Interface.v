ee4f3cd69462f73d8f5fc7f2627d98f939b5571d
Abhishek Dang <ahdang@iitk.ac.in>
2023-04-11 21:39:14 +0530

    unused class removed

--- src/Verse/Monoid/Interface.v
+++ src/Verse/Monoid/Interface.v
@@ -10,17 +10,3 @@ Record mSpecs ltypes mtypes
 
 Arguments mvariables [ltypes mtypes].
 Arguments mtypeCompiler [ltypes mtypes].
-
-Class Interface {ltypes mtypes}
-      (v : Variables.U ltypes)
-      (S : mSpecs ltypes mtypes)
-  := {
-      Var : forall {ty : some (typeOf ltypes)},
-        v ty -> Variables.Universe.embed (mvariables S)
-                                           (Types.Some.translate (mtypeCompiler S) ty)
-
-      (* This cannot use Universe.inject as if the typeCompiler
-         is a true compiler, a Var map would not be possible at
-         all!
-       *)
-  }.
